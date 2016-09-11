bdd81ac3079efe736e710da296f51dd797a8d713
Steven Sheldon <steven@sasheldon.com>
2016-09-10 21:56:53 -0700

    Stubbed out Parser.

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,42 @@ impl fmt::Debug for Int {
         fmt::Debug::fmt(&'i', formatter)
     }
 }
+
+trait ParserCompletion {
+    fn did_parse<E: 'static + Encoding>(self, encoding: E);
+}
+
+struct Parser<C> {
+    completion: C,
+}
+
+impl<C: ParserCompletion> Parser<C> {
+    fn parse(self, input: &str) {
+        self.completion.did_parse(Int);
+    }
+}
+
+struct BoxedEncodingCompletion<F> {
+    f: F,
+}
+
+impl<F> ParserCompletion for BoxedEncodingCompletion<F>
+        where F: FnOnce(Box<Encoding>) {
+    fn did_parse<E: 'static + Encoding>(self, encoding: E) {
+        (self.f)(Box::new(encoding))
+    }
+}
+
+pub fn parse(input: &str) -> Box<Encoding> {
+    let mut enc = None;
+    {
+        let enc_ref = &mut enc;
+        let callback = |e| {
+            *enc_ref = Some(e);
+        };
+        let completion = BoxedEncodingCompletion { f: callback };
+        let parser = Parser { completion: completion };
+        parser.parse(input);
+    }
+    enc.unwrap()
+}
