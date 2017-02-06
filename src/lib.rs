1ccbd313b2b9004fc0137f537be538a050f7152e
Steven Sheldon <steven@sasheldon.com>
2017-02-05 23:06:57 -0600

    Some madness for handling structs

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::fmt;
 pub trait Encoding: fmt::Display {
     fn as_primitive(&self) -> Option<Primitive> { None }
     fn as_pointer(&self) -> Option<(&Encoding, bool)> { None }
+    fn as_struct(&self) -> Option<StructDescriptor> { None }
 }
 
 #[derive(Clone, Copy)]
@@ -34,6 +35,7 @@ impl Encoding for Primitive {
 impl fmt::Display for Primitive {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         let s = match *self {
+            Primitive::Char => "c",
             Primitive::Int => "i",
             _ => panic!(),
         };
@@ -58,6 +60,81 @@ impl<T> fmt::Display for Pointer<T> where T: Encoding {
     }
 }
 
+pub trait EncodingTuple {
+    fn encoding_at(&self, i: u8) -> Option<&Encoding>;
+}
+
+impl<A, B> EncodingTuple for (A, B) where A: Encoding, B: Encoding {
+    fn encoding_at(&self, i: u8) -> Option<&Encoding> {
+        match i {
+            0 => Some(&self.0),
+            1 => Some(&self.1),
+            _ => None,
+        }
+    }
+}
+
+enum StructFields<'a> {
+    Static(&'a EncodingTuple),
+    Parsed(&'a str),
+}
+
+pub struct StructDescriptor<'a> {
+    name: &'a str,
+    fields: StructFields<'a>,
+    index: u8,
+    current: Option<Primitive>,
+}
+
+impl<'a> StructDescriptor<'a> {
+    pub fn new<'b>(name: &'b str, fields: &'b EncodingTuple) -> StructDescriptor<'b> {
+        StructDescriptor {
+            name: name,
+            fields: StructFields::Static(fields),
+            index: 0,
+            current: None,
+        }
+    }
+
+    fn parse<'b>(name: &'b str, fields: &'b str) -> StructDescriptor<'b> {
+        StructDescriptor {
+            name: name,
+            fields: StructFields::Parsed(fields),
+            index: 0,
+            current: None,
+        }
+    }
+
+    pub fn name(&self) -> &str {
+        self.name
+    }
+
+    pub fn next_field(&mut self) -> Option<&Encoding> {
+        let index = self.index;
+        self.index += 1;
+        match self.fields {
+            StructFields::Static(tup) => tup.encoding_at(index),
+            StructFields::Parsed(s) => {
+                let (enc, remaining) = parse(s);
+                self.current = enc;
+                self.fields = StructFields::Parsed(remaining);
+                self.current.as_ref().map(|e| e as &Encoding)
+            },
+        }
+    }
+}
+
+fn parse(s: &str) -> (Option<Primitive>, &str) {
+    if s.len() == 0 {
+        return (None, s);
+    }
+    match s.as_bytes()[0] {
+        b'c' => (Some(Primitive::Char), &s[1..]),
+        b'i' => (Some(Primitive::Int), &s[1..]),
+        _ => (None, s),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -71,5 +148,24 @@ mod tests {
     fn test_pointer_display() {
         let e = Pointer { t: Primitive::Int, is_const: false };
         assert_eq!(e.to_string(), "^i");
+    }
+
+    #[test]
+    fn test_static_struct() {
+        let f = (Primitive::Char, Primitive::Int);
+        let mut s = StructDescriptor::new("CGPoint", &f);
+        assert_eq!(s.name(), "CGPoint");
+        assert_eq!(s.next_field().unwrap().to_string(), "c");
+        assert_eq!(s.next_field().unwrap().to_string(), "i");
+        assert!(s.next_field().is_none());
+    }
+
+    #[test]
+    fn test_parsed_struct() {
+        let mut s = StructDescriptor::parse("CGPoint", "ci");
+        assert_eq!(s.name(), "CGPoint");
+        assert_eq!(s.next_field().unwrap().to_string(), "c");
+        assert_eq!(s.next_field().unwrap().to_string(), "i");
+        assert!(s.next_field().is_none());
     }
 }
