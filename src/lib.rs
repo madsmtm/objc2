e4e1dc055a8308970c062da9b17656a64a59320c
Steven Sheldon <steven@sasheldon.com>
2017-02-08 00:29:29 -0600

    Remove the rest of the pub mods.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 mod descriptor;
-pub mod encodings;
-pub mod parse;
+mod encodings;
+mod parse;
 
 use std::fmt;
 
 pub use descriptor::Descriptor;
+pub use encodings::{Primitive, Pointer, Struct, EncodingTuple};
+pub use parse::StrEncoding;
 
 pub trait Encoding: fmt::Display {
     fn descriptor(&self) -> Descriptor;
