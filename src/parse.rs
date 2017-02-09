409c009fabec45da8bf4eaa352e929c0382b2e32
Steven Sheldon <steven@sasheldon.com>
2017-02-09 11:21:38 -0600

    Parsing fields besides primitives now works

--- src/parse.rs
+++ src/parse.rs
@@ -4,9 +4,71 @@ use Encoding;
 use descriptor::{Descriptor, DescriptorKind, AnyEncoding, FieldsIterator};
 use encodings::Primitive;
 
+fn chomp(s: &str) -> (Option<&str>, &str) {
+    let head_len = chomp_ptr(s)
+        .or_else(|| chomp_struct(s))
+        .or_else(|| {
+            if let (Some(_), t) = chomp_primitive(s) {
+                Some(s.len() - t.len())
+            } else {
+                None
+            }
+        });
+
+    if let Some(head_len) = head_len {
+        let (h, t) = s.split_at(head_len);
+        (Some(h), t)
+    } else {
+        (None, s)
+    }
+}
+
+fn chomp_ptr(s: &str) -> Option<usize> {
+    if s.starts_with("^") {
+        let (h, _) = chomp(&s[1..]);
+        h.map(|h| h.len() + 1)
+    } else {
+        None
+    }
+}
+
+fn chomp_struct(s: &str) -> Option<usize> {
+    if !s.starts_with("{") {
+        return None;
+    }
+
+    let mut depth = 1;
+    for (i, b) in s.bytes().enumerate().skip(1) {
+        if b == b'{' {
+            depth += 1;
+        } else if b == b'}' {
+            depth -= 1;
+        }
+
+        if depth == 0 {
+            return Some(i + 1);
+        }
+    }
+
+    None
+}
+
+fn chomp_primitive(s: &str) -> (Option<Primitive>, &str) {
+    if s.is_empty() {
+        return (None, s);
+    }
+
+    let (h, t) = s.split_at(1);
+    match h {
+        "c" => (Some(Primitive::Char), t),
+        "i" => (Some(Primitive::Int), t),
+        _ => (None, s),
+    }
+}
+
 pub fn parse(s: &str) -> (Option<StrEncoding<&str>>, &str) {
-    if s.len() >= 1 {
-        (Some(StrEncoding { buf: &s[..1] }), &s[1..])
+    if let (Some(h), t) = chomp(s) {
+        (Some(StrEncoding::new_unchecked(h)), t)
     } else {
         (None, s)
     }
