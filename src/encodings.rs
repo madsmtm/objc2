aeee7bc43cc888e4649115ce69b2a414ed55251f
Steven Sheldon <steven@sasheldon.com>
2017-02-12 18:19:02 -0600

    Broke out code related to multiple encodings into a multi mod

--- src/encodings.rs
+++ src/encodings.rs
@@ -1,7 +1,8 @@
 use std::fmt;
 
-use {Encoding, PointerEncoding, StructEncoding, FieldsComparator};
+use {Encoding, PointerEncoding, StructEncoding};
 use descriptor::Descriptor;
+use multi::{EncodingTuple, EncodingTupleComparator, FieldsComparator};
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub enum Primitive {
@@ -108,63 +109,6 @@ impl<T> fmt::Display for Pointer<T> where T: Encoding {
     }
 }
 
-pub trait EncodingTuple {
-    fn eq<F: FieldsComparator>(&self, F) -> bool;
-
-    fn write_all<W: fmt::Write>(&self, &mut W) -> fmt::Result;
-
-    fn encoding_at_eq<T: ?Sized + Encoding>(&self, u8, &T) -> bool;
-
-    fn len(&self) -> u8;
-}
-
-impl<A, B> EncodingTuple for (A, B) where A: Encoding, B: Encoding {
-    fn eq<F: FieldsComparator>(&self, mut fields: F) -> bool {
-        fields.eq_next(&self.0) && fields.eq_next(&self.1) && fields.is_finished()
-    }
-
-    fn write_all<W: fmt::Write>(&self, formatter: &mut W) -> fmt::Result {
-        write!(formatter, "{}{}", self.0, self.1)
-    }
-
-    fn encoding_at_eq<T: ?Sized + Encoding>(&self, index: u8, other: &T) -> bool {
-        match index {
-            0 => self.0.eq_encoding(other),
-            1 => self.1.eq_encoding(other),
-            _ => false,
-        }
-    }
-
-    fn len(&self) -> u8 { 2 }
-}
-
-struct EncodingTupleComparator<'a, T> where T: 'a + EncodingTuple {
-    encs: &'a T,
-    index: u8,
-}
-
-impl<'a, T> EncodingTupleComparator<'a, T> where T: 'a + EncodingTuple {
-    fn new(encs: &'a T) -> EncodingTupleComparator<'a, T> {
-        EncodingTupleComparator { encs: encs, index: 0 }
-    }
-}
-
-impl<'a, T> FieldsComparator for EncodingTupleComparator<'a, T>
-        where T: 'a + EncodingTuple {
-    fn eq_next<E: ?Sized + Encoding>(&mut self, other: &E) -> bool {
-        let index = self.index;
-        if index < self.encs.len() {
-            self.index += 1;
-            self.encs.encoding_at_eq(index, other)
-        } else {
-            false
-        }
-    }
-
-    fn is_finished(&self) -> bool {
-        self.index >= self.encs.len()
-    }
-}
 
 pub struct Struct<S, T> where S: AsRef<str>, T: EncodingTuple {
     name: S,
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 mod descriptor;
 mod encodings;
+mod multi;
 mod parse;
 
 use std::fmt;
 
 pub use descriptor::Descriptor;
-pub use encodings::{Primitive, Pointer, Struct, EncodingTuple};
+pub use encodings::{Primitive, Pointer, Struct};
+pub use multi::{EncodingTuple, FieldsComparator};
 pub use parse::StrEncoding;
 
 pub trait Encoding: fmt::Display {
@@ -15,11 +17,6 @@ pub trait Encoding: fmt::Display {
     fn descriptor(&self) -> Descriptor<Self::Pointer, Self::Struct>;
 
     fn eq_encoding<T: ?Sized + Encoding>(&self, &T) -> bool;
-}
-
-pub trait FieldsComparator {
-    fn eq_next<T: ?Sized + Encoding>(&mut self, &T) -> bool;
-    fn is_finished(&self) -> bool;
 }
 
 pub trait StructEncoding: Encoding {
