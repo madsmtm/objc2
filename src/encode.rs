0cdd315c3723aadf385943f96ada7bf1ed9607bc
Steven Sheldon <steven@sasheldon.com>
2017-02-16 00:32:03 -0800

    Copy and adapt the Encode trait from objc

---
+++ src/encode.rs
@@ -0,0 +1,59 @@
+use Encoding;
+use encoding::Primitive;
+
+/// Types that have an Objective-C type encoding.
+///
+/// Unsafe because Objective-C will make assumptions about the type (like its
+/// size and alignment) from its encoding, so the implementer must verify that
+/// the encoding is accurate.
+pub unsafe trait Encode {
+    type Encoding: Encoding;
+
+    /// Returns the Objective-C type encoding for Self.
+    fn encode() -> Self::Encoding;
+}
+
+macro_rules! encode_impls {
+    ($($t:ty : $e:ident,)*) => ($(
+        unsafe impl Encode for $t {
+            type Encoding = Primitive;
+
+            fn encode() -> Primitive { Primitive::$e }
+        }
+    )*);
+}
+
+encode_impls!(
+    i8: Char,
+    i16: Short,
+    i32: Int,
+    i64: LongLong,
+    u8: UChar,
+    u16: UShort,
+    u32: UInt,
+    u64: ULongLong,
+    f32: Float,
+    f64: Double,
+    bool: Bool,
+    (): Void,
+);
+
+unsafe impl Encode for isize {
+    type Encoding = Primitive;
+
+    #[cfg(target_pointer_width = "32")]
+    fn encode() -> Primitive { i32::encode() }
+
+    #[cfg(target_pointer_width = "64")]
+    fn encode() -> Primitive { i64::encode() }
+}
+
+unsafe impl Encode for usize {
+    type Encoding = Primitive;
+
+    #[cfg(target_pointer_width = "32")]
+    fn encode() -> Primitive { u32::encode() }
+
+    #[cfg(target_pointer_width = "64")]
+    fn encode() -> Primitive { u64::encode() }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,12 @@
 extern crate std;
 
 mod encoding;
+mod encode;
 mod multi;
 mod parse;
 
 pub use encoding::{Descriptor, Encoding};
 pub use encoding::{Primitive, Pointer, Array, Struct, Union};
+pub use encode::Encode;
 pub use multi::{Encodings, EncodingsComparator, IndexEncodings};
 pub use parse::{StrEncoding, ParseEncodingError};
