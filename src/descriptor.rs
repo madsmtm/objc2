59c0ba6784d2ff235f5c0df07e40b55295c0eb93
Steven Sheldon <steven@sasheldon.com>
2017-02-19 12:24:30 -0800

    Pulled descriptor into its own module.

---
+++ src/descriptor.rs
@@ -0,0 +1,85 @@
+use core::fmt;
+
+use {Encoding, Encodings};
+use encoding::Primitive;
+
+/**
+A type which describes an `Encoding`.
+
+In a sense, descriptors allow a form of downcasting for `Encoding`s.
+While accepting an `Encoding` of any type, through its descriptor we can
+still know what kind it is and access specific information about it, like
+the length of an array or the name of a struct. This allows encodings of
+different types to be compared and interoperate.
+*/
+#[derive(Debug)]
+pub enum Descriptor<'a, T, I, F, M>
+        where T: 'a + ?Sized + Encoding,
+              I: 'a + ?Sized + Encoding,
+              F: 'a + ?Sized + Encodings,
+              M: 'a + ?Sized + Encodings {
+    Primitive(Primitive),
+    Pointer(&'a T),
+    Array(u32, &'a I),
+    Struct(&'a str, &'a F),
+    Union(&'a str, &'a M),
+}
+
+impl<'a, T, I, F, M> Copy for Descriptor<'a, T, I, F, M>
+        where T: 'a + ?Sized + Encoding,
+              I: 'a + ?Sized + Encoding,
+              F: 'a + ?Sized + Encodings,
+              M: 'a + ?Sized + Encodings { }
+
+impl<'a, T, I, F, M> Clone for Descriptor<'a, T, I, F, M>
+        where T: 'a + ?Sized + Encoding,
+              I: 'a + ?Sized + Encoding,
+              F: 'a + ?Sized + Encodings,
+              M: 'a + ?Sized + Encodings {
+    fn clone(&self) -> Descriptor<'a, T, I, F, M> {
+        *self
+    }
+}
+
+pub fn encodings_eq<T, U>(e1: &T, e2: &U) -> bool
+        where T: ?Sized + Encoding, U: ?Sized + Encoding {
+    use Descriptor::*;
+    match (e1.descriptor(), e2.descriptor()) {
+        (Primitive(p1), Primitive(p2)) => p1 == p2,
+        (Pointer(t1), Pointer(t2)) => t1.eq_encoding(t2),
+        (Array(l1, i1), Array(l2, i2)) =>
+            l1 == l2 && i1.eq_encoding(i2),
+        (Struct(n1, f1), Struct(n2, f2)) =>
+            n1 == n2 && f1.eq_encodings(f2),
+        (Union(n1, m1), Union(n2, m2)) =>
+            n1 == n2 && m1.eq_encodings(m2),
+        _ => false,
+    }
+}
+
+pub fn write_encoding<W, T>(writer: &mut W, encoding: &T) -> fmt::Result
+        where W: fmt::Write, T: ?Sized + Encoding {
+    use Descriptor::*;
+    match encoding.descriptor() {
+        Primitive(p) => write!(writer, "{}", p),
+        Pointer(t) => {
+            writer.write_char('^')?;
+            t.write(writer)
+        }
+        Array(len, item) => {
+            write!(writer, "[{}", len)?;
+            item.write(writer)?;
+            writer.write_char(']')
+        }
+        Struct(name, fields) => {
+            write!(writer, "{{{}=", name)?;
+            fields.write_all(writer)?;
+            writer.write_char('}')
+        }
+        Union(name, members) => {
+            write!(writer, "({}=", name)?;
+            members.write_all(writer)?;
+            writer.write_char(')')
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -64,12 +64,14 @@ extern crate std;
 
 pub mod encoding;
 mod encode;
+mod descriptor;
 mod multi;
 pub mod parse;
 
 use core::fmt;
 
 pub use encode::Encode;
+pub use descriptor::Descriptor;
 pub use multi::{Encodings, EncodingsIterateCallback};
 
 /// An Objective-C type encoding.
@@ -95,82 +97,11 @@ pub trait Encoding {
 
     /// Returns whether self is equal to the given `Encoding`.
     fn eq_encoding<T: ?Sized + Encoding>(&self, other: &T) -> bool {
-        use Descriptor::*;
-        match (self.descriptor(), other.descriptor()) {
-            (Primitive(p1), Primitive(p2)) => p1 == p2,
-            (Pointer(t1), Pointer(t2)) => t1.eq_encoding(t2),
-            (Array(l1, i1), Array(l2, i2)) =>
-                l1 == l2 && i1.eq_encoding(i2),
-            (Struct(n1, f1), Struct(n2, f2)) =>
-                n1 == n2 && f1.eq_encodings(f2),
-            (Union(n1, m1), Union(n2, m2)) =>
-                n1 == n2 && m1.eq_encodings(m2),
-            _ => false,
-        }
+        descriptor::encodings_eq(self, other)
     }
 
     /// Writes the string representation of self to the given writer.
     fn write<W: fmt::Write>(&self, writer: &mut W) -> fmt::Result {
-        use Descriptor::*;
-        match self.descriptor() {
-            Primitive(p) => write!(writer, "{}", p),
-            Pointer(t) => {
-                writer.write_char('^')?;
-                t.write(writer)
-            }
-            Array(len, item) => {
-                write!(writer, "[{}", len)?;
-                item.write(writer)?;
-                writer.write_char(']')
-            }
-            Struct(name, fields) => {
-                write!(writer, "{{{}=", name)?;
-                fields.write_all(writer)?;
-                writer.write_char('}')
-            }
-            Union(name, members) => {
-                write!(writer, "({}=", name)?;
-                members.write_all(writer)?;
-                writer.write_char(')')
-            }
-        }
-    }
-}
-
-/**
-A type which describes an `Encoding`.
-
-In a sense, descriptors allow a form of downcasting for `Encoding`s.
-While accepting an `Encoding` of any type, through its descriptor we can
-still know what kind it is and access specific information about it, like
-the length of an array or the name of a struct. This allows encodings of
-different types to be compared and interoperate.
-*/
-#[derive(Debug)]
-pub enum Descriptor<'a, T, I, F, M>
-        where T: 'a + ?Sized + Encoding,
-              I: 'a + ?Sized + Encoding,
-              F: 'a + ?Sized + Encodings,
-              M: 'a + ?Sized + Encodings {
-    Primitive(encoding::Primitive),
-    Pointer(&'a T),
-    Array(u32, &'a I),
-    Struct(&'a str, &'a F),
-    Union(&'a str, &'a M),
-}
-
-impl<'a, T, I, F, M> Copy for Descriptor<'a, T, I, F, M>
-        where T: 'a + ?Sized + Encoding,
-              I: 'a + ?Sized + Encoding,
-              F: 'a + ?Sized + Encodings,
-              M: 'a + ?Sized + Encodings { }
-
-impl<'a, T, I, F, M> Clone for Descriptor<'a, T, I, F, M>
-        where T: 'a + ?Sized + Encoding,
-              I: 'a + ?Sized + Encoding,
-              F: 'a + ?Sized + Encodings,
-              M: 'a + ?Sized + Encodings {
-    fn clone(&self) -> Descriptor<'a, T, I, F, M> {
-        *self
+        descriptor::write_encoding(writer, self)
     }
 }
