386895042fe2681cfdc33b6bd6fe2b77ae63c49c
Mads Marquart <mads@marquart.dk>
2021-09-02 15:50:27 +0200

    Move objc_test_utils stuff to match normal crate structure

--- objc_test_utils/build.rs
+++ objc_test_utils/build.rs
@@ -3,7 +3,7 @@ extern crate gcc;
 fn main() {
     gcc::Config::new()
         .compiler("clang")
-        .file("block_utils.c")
+        .file("extern/block_utils.c")
         .flag("-fblocks")
         .compile("libblock_utils.a");
 }
