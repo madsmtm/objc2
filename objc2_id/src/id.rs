e41d1c1ec82cbba446e6cbf180d7ba3324477048
Mads Marquart <mads@marquart.dk>
2021-09-09 12:50:01 +0200

    Make the Ownership trait sealed

--- objc2_id/src/id.rs
+++ objc2_id/src/id.rs
@@ -1,6 +1,5 @@
 use alloc::borrow;
 use alloc::boxed::Box;
-use core::any::Any;
 use core::cell::UnsafeCell;
 use core::fmt;
 use core::hash;
@@ -17,13 +16,25 @@ use objc2::Message;
 /// A type used to mark that a struct owns the object(s) it contains,
 /// so it has the sole references to them.
 pub enum Owned {}
+
 /// A type used to mark that the object(s) a struct contains are shared,
 /// so there may be other references to them.
 pub enum Shared {}
 
+mod private {
+    pub trait Sealed {}
+
+    impl Sealed for super::Owned {}
+    impl Sealed for super::Shared {}
+}
+
 /// A type that marks what type of ownership a struct has over the object(s)
 /// it contains; specifically, either [`Owned`] or [`Shared`].
-pub trait Ownership: Any {}
+///
+/// This trait is sealed and not meant to be implemented outside of the this
+/// crate.
+pub trait Ownership: private::Sealed + 'static {}
+
 impl Ownership for Owned {}
 impl Ownership for Shared {}
 
