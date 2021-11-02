02aa9cdfdbf1d12e1242b471cc8f97e63116fea0
Mads Marquart <mads@marquart.dk>
2021-11-02 15:42:40 +0100

    Ensure AutoreleasePool is !RefUnwindSafe
    Thought of when seeing the `mut_from_ref` clippy warning

--- objc2/src/rc/autorelease.rs
+++ objc2/src/rc/autorelease.rs
@@ -1,4 +1,6 @@
+use core::cell::UnsafeCell;
 use core::ffi::c_void;
+use core::marker::PhantomData;
 #[cfg(all(debug_assertions, not(feature = "unstable_autoreleasesafe")))]
 use std::{cell::RefCell, thread_local, vec::Vec};
 
@@ -15,6 +17,8 @@ use crate::runtime::{objc_autoreleasePoolPop, objc_autoreleasePoolPush};
 /// pool on the current thread.
 pub struct AutoreleasePool {
     context: *mut c_void,
+    // May pointer to data that is mutated (even though we hold shared access)
+    p: PhantomData<*mut UnsafeCell<c_void>>,
 }
 
 /// ```rust,compile_fail
@@ -55,7 +59,10 @@ impl AutoreleasePool {
         let context = objc_autoreleasePoolPush();
         #[cfg(all(debug_assertions, not(feature = "unstable_autoreleasesafe")))]
         POOLS.with(|c| c.borrow_mut().push(context));
-        Self { context }
+        Self {
+            context,
+            p: PhantomData,
+        }
     }
 
     /// This will be removed in a future version.
