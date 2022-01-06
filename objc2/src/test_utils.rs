SSheldon/rust-objc

Add a terminated dependency and use NulTerminatedStr for inputs

src/test_utils.rs
@@ -61,18 +61,18 @@ pub fn custom_class() -> &'static Class {
         // The runtime will call this method, so it has to be implemented
         extern fn custom_obj_class_initialize(_this: &Class, _cmd: Sel) { }
 
-        let mut decl = ClassDecl::root("CustomObject", custom_obj_class_initialize).unwrap();
+        let mut decl = ClassDecl::root(ntstr!("CustomObject"), custom_obj_class_initialize).unwrap();
         let proto = custom_protocol();
 
         decl.add_protocol(proto);
-        decl.add_ivar::<u32>("_foo");
+        decl.add_ivar::<u32>(ntstr!("_foo"));
 
         extern fn custom_obj_set_foo(this: &mut Object, _cmd: Sel, foo: u32) {
-            unsafe { this.set_ivar::<u32>("_foo", foo); }
+            unsafe { this.set_ivar::<u32>(ntstr!("_foo"), foo); }
         }
 
         extern fn custom_obj_get_foo(this: &Object, _cmd: Sel) -> u32 {
-            unsafe { *this.get_ivar::<u32>("_foo") }
+            unsafe { *this.get_ivar::<u32>(ntstr!("_foo")) }
         }
 
         extern fn custom_obj_get_struct(_this: &Object, _cmd: Sel) -> CustomStruct {
@@ -84,7 +84,7 @@ pub fn custom_class() -> &'static Class {
         }
 
         extern fn custom_obj_set_bar(this: &mut Object, _cmd: Sel, bar: u32) {
-            unsafe { this.set_ivar::<u32>("_foo", bar) ;}
+            unsafe { this.set_ivar::<u32>(ntstr!("_foo"), bar) ;}
         }
 
         extern fn custom_obj_add_number_to_number(_this: &Class, _cmd: Sel, fst: i32, snd: i32) -> i32 {
@@ -117,7 +117,7 @@ pub fn custom_protocol() -> &'static Protocol {
     static REGISTER_CUSTOM_PROTOCOL: Once = ONCE_INIT;
 
     REGISTER_CUSTOM_PROTOCOL.call_once(|| {
-        let mut decl = ProtocolDecl::new("CustomProtocol").unwrap();
+        let mut decl = ProtocolDecl::new(ntstr!("CustomProtocol")).unwrap();
 
         decl.add_method_description::<(i32,), ()>(sel!(setBar:), true);
         decl.add_method_description::<(), *const c_char>(sel!(getName), false);
@@ -126,7 +126,7 @@ pub fn custom_protocol() -> &'static Protocol {
         decl.register();
     });
 
-    Protocol::get("CustomProtocol").unwrap()
+    Protocol::get(ntstr!("CustomProtocol")).unwrap()
 }
 
 pub fn custom_subprotocol() -> &'static Protocol {
@@ -134,7 +134,7 @@ pub fn custom_subprotocol() -> &'static Protocol {
 
     REGISTER_CUSTOM_SUBPROTOCOL.call_once(|| {
         let super_proto = custom_protocol();
-        let mut decl = ProtocolDecl::new("CustomSubProtocol").unwrap();
+        let mut decl = ProtocolDecl::new(ntstr!("CustomSubProtocol")).unwrap();
 
         decl.add_protocol(super_proto);
         decl.add_method_description::<(u32,), u32>(sel!(calculateFoo:), true);
@@ -142,7 +142,7 @@ pub fn custom_subprotocol() -> &'static Protocol {
         decl.register();
     });
 
-    Protocol::get("CustomSubProtocol").unwrap()
+    Protocol::get(ntstr!("CustomSubProtocol")).unwrap()
 }
 
 pub fn custom_object() -> CustomObject {
@@ -154,7 +154,7 @@ pub fn custom_subclass() -> &'static Class {
 
     REGISTER_CUSTOM_SUBCLASS.call_once(|| {
         let superclass = custom_class();
-        let mut decl = ClassDecl::new("CustomSubclassObject", superclass).unwrap();
+        let mut decl = ClassDecl::new(ntstr!("CustomSubclassObject"), superclass).unwrap();
 
         extern fn custom_subclass_get_foo(this: &Object, _cmd: Sel) -> u32 {
             let foo: u32 = unsafe {

madsmtm/objc2

objc2/src/test_utils.rs
@@ -1,6 +1,7 @@
 use core::ops::{Deref, DerefMut};
 use std::os::raw::c_char;
 use std::sync::Once;
+use terminated::ntstr;
 
 use crate::declare::{ClassDecl, ProtocolDecl};
 use crate::runtime::{Class, Object, Protocol, Sel};
@@ -75,20 +76,21 @@ pub(crate) fn custom_class() -> &'static Class {
         // The runtime will call this method, so it has to be implemented
         extern "C" fn custom_obj_class_initialize(_this: &Class, _cmd: Sel) {}
 
-        let mut decl = ClassDecl::root("CustomObject", custom_obj_class_initialize).unwrap();
+        let mut decl =
+            ClassDecl::root(ntstr!("CustomObject"), custom_obj_class_initialize).unwrap();
         let proto = custom_protocol();
 
         decl.add_protocol(proto);
-        decl.add_ivar::<u32>("_foo");
+        decl.add_ivar::<u32>(ntstr!("_foo"));
 
         extern "C" fn custom_obj_set_foo(this: &mut Object, _cmd: Sel, foo: u32) {
             unsafe {
-                this.set_ivar::<u32>("_foo", foo);
+                this.set_ivar::<u32>(ntstr!("_foo"), foo);
             }
         }
 
         extern "C" fn custom_obj_get_foo(this: &Object, _cmd: Sel) -> u32 {
-            unsafe { *this.ivar::<u32>("_foo") }
+            unsafe { *this.ivar::<u32>(ntstr!("_foo")) }
         }
 
         extern "C" fn custom_obj_get_struct(_this: &Object, _cmd: Sel) -> CustomStruct {
@@ -106,7 +108,7 @@ pub(crate) fn custom_class() -> &'static Class {
 
         extern "C" fn custom_obj_set_bar(this: &mut Object, _cmd: Sel, bar: u32) {
             unsafe {
-                this.set_ivar::<u32>("_foo", bar);
+                this.set_ivar::<u32>(ntstr!("_foo"), bar);
             }
         }
 
@@ -146,7 +148,7 @@ pub(crate) fn custom_protocol() -> &'static Protocol {
     static REGISTER_CUSTOM_PROTOCOL: Once = Once::new();
 
     REGISTER_CUSTOM_PROTOCOL.call_once(|| {
-        let mut decl = ProtocolDecl::new("CustomProtocol").unwrap();
+        let mut decl = ProtocolDecl::new(ntstr!("CustomProtocol")).unwrap();
 
         decl.add_method_description::<(i32,), ()>(sel!(setBar:), true);
         decl.add_method_description::<(), *const c_char>(sel!(getName), false);
@@ -155,7 +157,7 @@ pub(crate) fn custom_protocol() -> &'static Protocol {
         decl.register();
     });
 
-    Protocol::get("CustomProtocol").unwrap()
+    Protocol::get(ntstr!("CustomProtocol")).unwrap()
 }
 
 pub(crate) fn custom_subprotocol() -> &'static Protocol {
@@ -163,7 +165,7 @@ pub(crate) fn custom_subprotocol() -> &'static Protocol {
 
     REGISTER_CUSTOM_SUBPROTOCOL.call_once(|| {
         let super_proto = custom_protocol();
-        let mut decl = ProtocolDecl::new("CustomSubProtocol").unwrap();
+        let mut decl = ProtocolDecl::new(ntstr!("CustomSubProtocol")).unwrap();
 
         decl.add_protocol(super_proto);
         decl.add_method_description::<(u32,), u32>(sel!(calculateFoo:), true);
@@ -171,7 +173,7 @@ pub(crate) fn custom_subprotocol() -> &'static Protocol {
         decl.register();
     });
 
-    Protocol::get("CustomSubProtocol").unwrap()
+    Protocol::get(ntstr!("CustomSubProtocol")).unwrap()
 }
 
 pub(crate) fn custom_object() -> CustomObject {
@@ -183,7 +185,7 @@ pub(crate) fn custom_subclass() -> &'static Class {
 
     REGISTER_CUSTOM_SUBCLASS.call_once(|| {
         let superclass = custom_class();
-        let mut decl = ClassDecl::new("CustomSubclassObject", superclass).unwrap();
+        let mut decl = ClassDecl::new(ntstr!("CustomSubclassObject"), superclass).unwrap();
 
         extern "C" fn custom_subclass_get_foo(this: &Object, _cmd: Sel) -> u32 {
             let foo: u32 = unsafe { msg_send![super(this, custom_class()), foo] };