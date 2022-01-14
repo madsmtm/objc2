c3b6733f3632a66c410a4fe782fcc0acb1a0273a
Mads Marquart <mads@marquart.dk>
2022-01-14 05:12:42 +0100

    Make compile tests much more robust

--- compile-tests/src/main.rs
+++ compile-tests/src/main.rs
@@ -1,63 +1,61 @@
+use cargo_metadata::camino::Utf8PathBuf;
+use cargo_metadata::Message;
 use std::env;
 use std::io;
-use std::path::{Path, PathBuf};
+use std::path::Path;
+use std::process::Command;
+use std::process::Stdio;
 
 use compiletest_rs::{common::Mode, run_tests, Config};
 
-// Environment variables created in build script
-const TARGET: &'static str = env!("BUILD_TARGET");
-const PROFILE: &'static str = env!("BUILD_PROFILE");
-
-fn find_dependency(deps_dir: &Path, dep: &str) -> io::Result<PathBuf> {
-    // Find lib[dep]-XYZ.rlib
-    // Yes. This is ugly.
-    let mut rlib = None;
-    for dir in deps_dir.read_dir()? {
-        let path = dir?.path();
-        if path
-            .file_name()
-            .unwrap()
-            .to_str()
-            .unwrap()
-            .starts_with(&format!("lib{}-", dep))
-            && path.extension().map_or(false, |ext| ext == "rlib")
-        {
-            if let Some(rlib) = rlib {
-                panic!("Found multiple rlibs: {:?}, {:?}", rlib, path);
-            } else {
-                rlib = Some(path);
-            }
-        }
-    }
-    Ok(rlib.expect("Found no rlib"))
+fn get_rlib<'a>(filenames: impl IntoIterator<Item = &'a Utf8PathBuf>) -> &'a Utf8PathBuf {
+    filenames
+        .into_iter()
+        .find(|name| name.extension() == Some("rlib"))
+        .expect("An rlib")
 }
 
-fn run(src: &'static str, mode: Mode) {
+fn main() -> io::Result<()> {
+    let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+
+    // Build objc2
+    let result = Command::new("cargo")
+        .current_dir(&manifest_dir.parent().unwrap())
+        .arg("build")
+        .arg("-pobjc2")
+        .arg("--message-format=json-render-diagnostics")
+        .args(std::env::args().skip(1))
+        .stdout(Stdio::piped())
+        .output()?;
+
+    // Extract metadata from build
+    let artifacts: Vec<_> = cargo_metadata::Message::parse_stream(&*result.stdout)
+        .filter_map(|message| {
+            if let Message::CompilerArtifact(artifact) = message.unwrap() {
+                if artifact.target.kind == ["lib"] && !artifact.profile.test {
+                    return Some(artifact);
+                }
+            }
+            None
+        })
+        .collect();
+    let dep_dir = get_rlib(&artifacts[0].filenames).parent().unwrap();
+    let flags = artifacts
+        .iter()
+        .map(|artifact| {
+            format!(
+                " --extern {name}={rlib}",
+                name = artifact.target.name,
+                rlib = get_rlib(&artifact.filenames),
+            )
+        })
+        .collect::<String>();
+
     let mut config = Config::default();
-
-    // ../target
-    let target_dir = Path::new(env!("CARGO_MANIFEST_DIR"))
-        .parent()
-        .unwrap()
-        .join("target");
-
-    // [/TARGET]/debug/deps
-    let deps_dir = if target_dir.join(TARGET).exists() {
-        target_dir.join(TARGET)
-    } else {
-        target_dir
-    }
-    .join(PROFILE)
-    .join("deps");
-
-    let dir = find_dependency(&deps_dir, "objc2").unwrap();
-
-    config.mode = mode;
-    config.src_base = PathBuf::from(format!("{}/{}", env!("CARGO_MANIFEST_DIR"), src));
     config.target_rustcflags = Some(format!(
-        "-L dependency={} --extern objc2={}",
-        deps_dir.display(),
-        dir.display()
+        "-L dependency={dep}{flags}",
+        dep = dep_dir,
+        flags = flags,
     ));
     config.llvm_filecheck = Some(
         env::var("FILECHECK")
@@ -65,14 +63,24 @@ fn run(src: &'static str, mode: Mode) {
             .into(),
     );
     config.edition = Some("2018".into());
-    config.verbose = true;
+    config.verbose = matches!(
+        std::env::var("CARGO_TERM_VERBOSE").as_deref(),
+        Ok("true" | "1")
+    ) || std::env::args().any(|val| val == "--verbose" || val == "-v");
 
+    // Run UI tests
+    config.src_base = manifest_dir.join("ui");
+    config.mode = Mode::Ui;
     run_tests(&config);
-}
+    config.mode = Mode::CompileFail;
+    run_tests(&config);
 
-fn main() {
-    run("ui", Mode::Ui);
-    run("ui", Mode::CompileFail);
-    run("codegen", Mode::Codegen);
-    run("codegen", Mode::RunPass);
+    // Run Codegen tests
+    config.src_base = manifest_dir.join("codegen");
+    config.mode = Mode::RunPass;
+    run_tests(&config);
+    config.mode = Mode::Codegen;
+    run_tests(&config);
+
+    Ok(())
 }
