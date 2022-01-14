4794e5ce1c586c4547fd0bb72aa80f8f1aec80e1
Mads Marquart <mads@marquart.dk>
2022-01-14 02:07:13 +0100

    Fix msg_send_no_return_type test

--- objc2/tests/ui/msg_send_no_return_type.rs
+++ objc2/tests/ui/msg_send_no_return_type.rs
@@ -1,8 +1,10 @@
+// Test that forgetting to annotate the return type fails
+// See https://github.com/SSheldon/rust-objc/issues/62
 use objc2::{class, msg_send};
 
 fn main() {
     unsafe {
         let cls = class!(NSObject);
-        let _obj = msg_send![cls, new];
+        msg_send![cls, new];
     }
 }
