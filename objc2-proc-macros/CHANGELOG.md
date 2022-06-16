0e7162fdfac94ad381efd710c8f890648dea58c1
Mads Marquart <mads@marquart.dk>
2022-06-16 11:36:49 +0200

    Add empty proc-macro `objc2-proc-macros` for use in the future

---
+++ objc2-proc-macros/CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+Notable changes to this crate will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
+
+## Unreleased - YYYY-MM-DD
+
+
+## 0.0.0 - 2022-06-16
+
+Initial empty release.
---
+++ objc2-proc-macros/src/lib.rs
@@ -0,0 +1,18 @@
+//! Procedural macros for the [`objc2` project](https://github.com/madsmtm/objc2).
+//!
+//! You should not need to use this crate directly, all its public items are
+//! exported in other crates.
+
+#![warn(elided_lifetimes_in_paths)]
+#![warn(missing_docs)]
+#![deny(non_ascii_idents)]
+#![warn(unreachable_pub)]
+#![deny(unsafe_op_in_unsafe_fn)]
+#![warn(clippy::cargo)]
+#![warn(clippy::ptr_as_ptr)]
+// Update in Cargo.toml as well.
+#![doc(html_root_url = "https://docs.rs/objc2-proc-macros/0.0.0")]
+
+#[cfg(doctest)]
+#[doc = include_str!("../README.md")]
+extern "C" {}
