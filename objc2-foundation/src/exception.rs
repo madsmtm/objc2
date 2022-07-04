45655c804340dd9116b8738ce2f90b2e5ea6d8a2
Mads Marquart <mads@marquart.dk>
2022-07-04 23:55:53 +0200

    Add NSException

---
+++ objc2-foundation/src/exception.rs
@@ -0,0 +1,132 @@
+use core::hint::unreachable_unchecked;
+
+use objc2::rc::{Id, Shared};
+use objc2::runtime::Object;
+use objc2::{msg_send, msg_send_id};
+
+use crate::{NSCopying, NSDictionary, NSObject, NSString};
+
+object! {
+    /// A special condition that interrupts the normal flow of program
+    /// execution.
+    ///
+    /// Exceptions can be thrown and caught using the `objc2::exception`
+    /// module.
+    ///
+    /// See also [Apple's documentation][doc].
+    ///
+    /// [doc]: https://developer.apple.com/documentation/foundation/nsexception?language=objc
+    unsafe pub struct NSException: NSObject;
+}
+
+// SAFETY: Exception objects are immutable data containers, and documented as
+// thread safe.
+unsafe impl Sync for NSException {}
+unsafe impl Send for NSException {}
+
+type NSExceptionName = NSString;
+
+impl NSException {
+    /// Create a new [`NSException`] object.
+    ///
+    /// Returns `None` if the exception couldn't be created (example: If the
+    /// process is out of memory).
+    pub fn new(
+        name: &NSExceptionName,
+        reason: Option<&NSString>,
+        user_info: Option<&NSDictionary<Object, Object>>,
+    ) -> Option<Id<Self, Shared>> {
+        let obj = unsafe { msg_send_id![Self::class(), alloc] };
+        unsafe { msg_send_id![obj, initWithName: name, reason: reason, userInfo: user_info] }
+    }
+
+    /// Raises the exception, causing program flow to jump to the local
+    /// exception handler.
+    ///
+    /// This is equivalent to using `objc2::exception::throw`.
+    ///
+    ///
+    /// # Safety
+    ///
+    /// Same as `objc2::exception::throw`.
+    pub unsafe fn raise(&self) -> ! {
+        // SAFETY: We only create `Shared` NSExceptions, so it is safe to give
+        // to the place where `@catch` receives it.
+        let _: () = unsafe { msg_send![self, raise] };
+        unsafe { unreachable_unchecked() }
+    }
+
+    /// A that uniquely identifies the type of exception.
+    ///
+    /// See [Apple's documentation][doc] for some of the different values this
+    /// can take.
+    ///
+    /// [doc]: https://developer.apple.com/documentation/foundation/nsexceptionname?language=objc
+    pub fn name(&self) -> Id<NSExceptionName, Shared> {
+        // Nullability not documented, but a name is expected in most places.
+        unsafe { msg_send_id![self, name].unwrap() }
+    }
+
+    /// A human-readable message summarizing the reason for the exception.
+    pub fn reason(&self) -> Option<Id<NSString, Shared>> {
+        unsafe { msg_send_id![self, reason] }
+    }
+
+    /// Application-specific data pertaining to the exception.
+    pub fn user_info(&self) -> Option<Id<NSDictionary<Object, Object>, Shared>> {
+        unsafe { msg_send_id![self, userInfo] }
+    }
+}
+
+unsafe impl NSCopying for NSException {
+    type Ownership = Shared;
+    type Output = NSException;
+}
+
+impl alloc::borrow::ToOwned for NSException {
+    type Owned = Id<NSException, Shared>;
+    fn to_owned(&self) -> Self::Owned {
+        self.copy()
+    }
+}
+
+// TODO: Better Debug impl
+
+#[cfg(test)]
+mod tests {
+    use alloc::format;
+
+    use super::*;
+
+    #[test]
+    fn create_and_query() {
+        let exc = NSException::new(
+            &NSString::from_str("abc"),
+            Some(&NSString::from_str("def")),
+            None,
+        )
+        .unwrap();
+
+        assert_eq!(exc.name(), NSString::from_str("abc"));
+        assert_eq!(exc.reason().unwrap(), NSString::from_str("def"));
+        assert!(exc.user_info().is_none());
+
+        assert_eq!(exc.description(), NSString::from_str("def"));
+        assert_eq!(format!("{:?}", exc), "\"def\"");
+    }
+
+    #[test]
+    #[should_panic = "called `Result::unwrap()` on an `Err` value: \"def\""]
+    fn unwrap() {
+        let exc = NSException::new(
+            &NSString::from_str("abc"),
+            Some(&NSString::from_str("def")),
+            None,
+        )
+        .unwrap();
+
+        let _: () = Err(exc).unwrap();
+    }
+
+    // Further tests in `tests::exception`
+}
---
+++ tests/src/exception.rs
@@ -0,0 +1,112 @@
+use objc2::exception::{catch, throw};
+use objc2::msg_send;
+use objc2::rc::{autoreleasepool, Id};
+use objc2::runtime::Object;
+use objc2_foundation::{NSArray, NSException, NSString};
+
+#[track_caller]
+fn assert_retain_count(obj: &Object, expected: usize) {
+    let retain_count: usize = unsafe { msg_send![obj, retainCount] };
+    assert_eq!(retain_count, expected);
+}
+
+#[test]
+#[cfg_attr(
+    feature = "catch_all",
+    ignore = "Panics inside `catch` when catch_all is enabled"
+)]
+fn throw_catch_raise_catch() {
+    let name = NSString::from_str("abc");
+    let reason = NSString::from_str("def");
+
+    let exc = NSException::new(&name, Some(&reason), None).unwrap();
+    assert_retain_count(&exc, 1);
+
+    let exc = autoreleasepool(|_| {
+        let res = unsafe {
+            catch(|| {
+                let exc = exc; // Move into closure
+                let exc = Id::cast::<Object>(exc);
+                throw(Some(&exc))
+                // exc is dropped on unwind here
+            })
+        };
+        let exc = res.unwrap_err().unwrap();
+        let exc = unsafe { Id::cast::<NSException>(exc) };
+
+        assert_retain_count(&exc, 2);
+        exc
+    });
+
+    assert_retain_count(&exc, 1);
+
+    assert_eq!(exc.name(), name);
+    assert_eq!(exc.reason().unwrap(), reason);
+    assert!(exc.user_info().is_none());
+}
+
+#[test]
+#[cfg(feature = "catch_all")]
+// Intentionally truncated panic message
+#[should_panic = "uncaught exception <NSException: 0x"]
+fn raise_catch_all() {
+    let name = NSString::from_str("abc");
+    let reason = NSString::from_str("def");
+
+    let exc = NSException::new(&name, Some(&reason), None).unwrap();
+    unsafe { exc.raise() };
+}
+
+#[test]
+#[cfg_attr(
+    feature = "catch_all",
+    ignore = "Panics inside `catch` when catch_all is enabled"
+)]
+// Intentionally truncated panic message
+#[should_panic = "called `Result::unwrap()` on an `Err` value: Some(<NSException: 0x"]
+fn raise_catch_unwrap() {
+    let name = NSString::from_str("abc");
+    let reason = NSString::from_str("def");
+
+    let exc = NSException::new(&name, Some(&reason), None).unwrap();
+    assert_retain_count(&exc, 1);
+
+    let res = autoreleasepool(|_| {
+        let res = unsafe {
+            catch(|| {
+                if exc.name() == name {
+                    exc.raise();
+                } else {
+                    4
+                }
+            })
+        };
+        assert_retain_count(&exc, 3);
+        res
+    });
+
+    assert_retain_count(&exc, 2);
+
+    let _ = res.unwrap();
+}
+
+#[test]
+#[cfg_attr(
+    feature = "catch_all",
+    ignore = "Panics inside `catch` when catch_all is enabled"
+)]
+fn catch_actual() {
+    let res = unsafe {
+        catch(|| {
+            let arr: Id<NSArray<Object, _>, _> = NSArray::new();
+            let _obj: *mut Object = msg_send![&arr, objectAtIndex: 0usize];
+        })
+    };
+    let exc = res.unwrap_err().unwrap();
+    let exc = unsafe { Id::cast::<NSException>(exc) };
+
+    assert_eq!(exc.name(), NSString::from_str("NSRangeException"));
+    let reason = "*** -[__NSArray0 objectAtIndex:]: index 0 beyond bounds for empty NSArray";
+    assert_eq!(exc.reason().unwrap(), NSString::from_str(reason));
+    assert!(exc.user_info().is_none());
+}
