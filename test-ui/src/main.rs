b717e4c2700cfc332d53c3a10d68361ecd3c223e
Mads Marquart <mads@marquart.dk>
2022-07-21 23:46:01 +0200

    Refactor UI tests
    Done so that when running locally, I don't have to build `trybuild` twice!

---
+++ test-ui/src/main.rs
@@ -0,0 +1,21 @@
+//! Run UI tests.
+//!
+//! Use as:
+//! ```
+//! TRYBUILD=overwrite cargo +nightly run --bin test-ui
+//! ```
+//!
+//! These are not part of the normal test suite, because trybuild doesn't pass
+//! feature flags (like --features gnustep-1-9) to its cargo invocation, and
+//! hence they don't always work.
+//!
+//! Also, they're slower than most tests.
+use std::path::Path;
+
+fn main() {
+    let t = trybuild::TestCases::new();
+    let path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("ui")
+        .join("*.rs");
+    t.compile_fail(path);
+}
