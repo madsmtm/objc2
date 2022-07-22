893e04f433d39b8ab7430e6f4beed3329b8950f3
Mads Marquart <mads@marquart.dk>
2022-07-22 03:26:34 +0200

    Add more NSUUID functionality

--- objc2-foundation/src/uuid.rs
+++ objc2-foundation/src/uuid.rs
@@ -1,5 +1,6 @@
 use core::panic::{RefUnwindSafe, UnwindSafe};
 
+use objc2::rc::DefaultId;
 use objc2::rc::{Id, Shared};
 use objc2::{msg_send, msg_send_id, Encode, Encoding, RefEncode};
 
@@ -9,6 +10,9 @@ extern_class! {
     /// A universally unique value.
     ///
     /// Can be used to identify types, interfaces, and other items.
+    ///
+    /// Conversion methods to/from UUIDs from the `uuid` crate can be
+    /// enabled with the `uuid` crate feature.
     ///
     /// See [Apple's documentation](https://developer.apple.com/documentation/foundation/nsuuid?language=objc).
     #[derive(Debug, PartialEq, Eq, Hash)]
@@ -35,10 +39,13 @@ impl UnwindSafe for NSUUID {}
 impl RefUnwindSafe for NSUUID {}
 
 impl NSUUID {
-    // TODO: `nil` method?
-
     pub fn new_v4() -> Id<Self, Shared> {
         unsafe { msg_send_id![Self::class(), new].unwrap() }
+    }
+
+    /// The 'nil UUID'.
+    pub fn nil() -> Id<Self, Shared> {
+        Self::from_bytes([0; 16])
     }
 
     pub fn from_bytes(bytes: [u8; 16]) -> Id<Self, Shared> {
@@ -49,7 +56,12 @@ impl NSUUID {
         }
     }
 
-    // TODO: `parse_str` using initWithUUIDString:
+    pub fn from_string(string: &NSString) -> Option<Id<Self, Shared>> {
+        unsafe {
+            let obj = msg_send_id![Self::class(), alloc];
+            msg_send_id![obj, initWithUUIDString: string]
+        }
+    }
 
     pub fn as_bytes(&self) -> [u8; 16] {
         let mut bytes = UuidBytes([0; 16]);
@@ -58,11 +70,40 @@ impl NSUUID {
     }
 }
 
+// UUID `compare:` is broken for some reason?
+
+// impl PartialOrd for NSUUID {
+//     #[inline]
+//     fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+//         Some(self.cmp(other))
+//     }
+// }
+
+// impl Ord for NSUUID {
+//     fn cmp(&self, other: &Self) -> cmp::Ordering {
+//         let res: NSComparisonResult = unsafe { msg_send![self, compare: other] };
+//         res.into()
+//     }
+// }
+
 /// Conversion methods to/from `uuid` crate.
 #[cfg(feature = "uuid")]
-// TODO: Research how stable the `uuid` crate is (or if we'll have to update
-// it constantly).
-impl NSUUID {}
+impl NSUUID {
+    pub fn from_uuid(uuid: uuid::Uuid) -> Id<Self, Shared> {
+        Self::from_bytes(uuid.into_bytes())
+    }
+
+    pub fn as_uuid(&self) -> uuid::Uuid {
+        uuid::Uuid::from_bytes(self.as_bytes())
+    }
+}
+
+impl DefaultId for NSUUID {
+    type Ownership = Shared;
+    fn default_id() -> Id<Self, Self::Ownership> {
+        Self::nil()
+    }
+}
 
 unsafe impl NSCopying for NSUUID {
     type Ownership = Shared;
@@ -91,5 +132,21 @@ mod tests {
     fn test_bytes() {
         let uuid = NSUUID::from_bytes([10; 16]);
         assert_eq!(uuid.as_bytes(), [10; 16]);
+    }
+
+    // #[test]
+    // fn test_compare() {
+    //     let uuid1 = NSUUID::from_bytes([10; 16]);
+    //     let uuid2 = NSUUID::from_bytes([9; 16]);
+    //     assert!(uuid1 > uuid2);
+    // }
+
+    #[cfg(feature = "uuid")]
+    #[test]
+    fn test_convert_roundtrip() {
+        let nsuuid1 = NSUUID::new_v4();
+        let uuid = nsuuid1.as_uuid();
+        let nsuuid2 = NSUUID::from_uuid(uuid);
+        assert_eq!(nsuuid1, nsuuid2);
     }
 }
