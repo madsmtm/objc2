8b9df64a3bb02be48ef48e5579393ec13afc5cd8
Mads Marquart <mads@marquart.dk>
2022-07-26 18:00:41 +0200

    Refactor `Ivar<T>` a bit

--- objc2/src/declare/ivar.rs
+++ objc2/src/declare/ivar.rs
@@ -123,11 +123,8 @@ pub struct Ivar<T: IvarType> {
     item: PhantomData<T::Type>,
 }
 
-impl<T: IvarType> Deref for Ivar<T> {
-    type Target = T::Type;
-
-    #[inline]
-    fn deref(&self) -> &Self::Target {
+impl<T: IvarType> Ivar<T> {
+    fn get_ref(&self) -> &T::Type {
         // SAFETY: The user ensures that this is placed in a struct that can
         // be reinterpreted as an `Object`. Since `Ivar` can never be
         // constructed by itself (and is neither Copy nor Clone), we know that
@@ -146,13 +143,10 @@ impl<T: IvarType> Deref for Ivar<T> {
         // exists and has the correct type
         unsafe { obj.ivar::<T::Type>(T::NAME) }
     }
-}
 
-impl<T: IvarType> DerefMut for Ivar<T> {
-    #[inline]
-    fn deref_mut(&mut self) -> &mut Self::Target {
+    fn get_mut_ptr(&mut self) -> *mut T::Type {
         let ptr = NonNull::from(self).cast::<Object>();
-        // SAFETY: Same as `deref`.
+        // SAFETY: Same as `get_ref`.
         //
         // Note: We don't use `mut` because the user might have two mutable
         // references to different ivars, as such:
@@ -177,12 +171,33 @@ impl<T: IvarType> DerefMut for Ivar<T> {
         // this is definitely safe.
         let obj = unsafe { ptr.as_ref() };
 
-        // SAFETY: Same as `deref`
-        //
-        // Safe as mutable because there is only one access to a particular
-        // ivar at a time (since we have `&mut self`). `Object` is
+        // SAFETY: User ensures that the `Ivar<T>` is only used when the ivar
+        // exists and has the correct type
+        unsafe { obj.ivar_ptr::<T::Type>(T::NAME) }
+    }
+
+    #[inline]
+    fn get_mut(&mut self) -> &mut T::Type {
+        // SAFETY: Safe as mutable because there is only one access to a
+        // particular ivar at a time (since we have `&mut self`). `Object` is
         // `UnsafeCell`, so mutable access through `&Object` is allowed.
-        unsafe { obj.ivar_ptr::<T::Type>(T::NAME).as_mut().unwrap_unchecked() }
+        unsafe { self.get_mut_ptr().as_mut().unwrap_unchecked() }
+    }
+}
+
+impl<T: IvarType> Deref for Ivar<T> {
+    type Target = T::Type;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        self.get_ref()
+    }
+}
+
+impl<T: IvarType> DerefMut for Ivar<T> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.get_mut()
     }
 }
 
