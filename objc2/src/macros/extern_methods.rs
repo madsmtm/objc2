0490fc11e82b82d129c40086ce9ba96f05122b5d
Mads Marquart <mads@marquart.dk>
2022-08-28 20:54:01 +0200

    Refactor __collect_msg_send! macro

--- objc2/src/macros/extern_methods.rs
+++ objc2/src/macros/extern_methods.rs
@@ -316,14 +316,14 @@ macro_rules! __inner_extern_methods {
             $self:ident: $self_ty:ty,
             _: $sel_ty:ty,
         )
-        @($($arg:ident: $arg_ty:ty),* $(,)?)
+        @($($args_rest:tt)*)
         @($($sel:tt)*)
     } => {
         $crate::__collect_msg_send!(
             $crate::msg_send;
             $self;
             ($($sel)*);
-            ($($arg),*);
+            ($($args_rest)*);
         )
     };
     {
@@ -333,205 +333,62 @@ macro_rules! __inner_extern_methods {
             _: $cls_ty:ty,
             _: $sel_ty:ty,
         )
-        @($($arg:ident: $arg_ty:ty),* $(,)?)
+        @($($args_rest:tt)*)
         @($($sel:tt)*)
     } => {
         $crate::__collect_msg_send!(
             $crate::msg_send;
             Self::class();
             ($($sel)*);
-            ($($arg),*);
+            ($($args_rest)*);
         )
     };
 }
 
 /// Zip selector and arguments, and forward to macro.
-///
-/// TODO: Investigate if there's a better way of doing this.
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __collect_msg_send {
+    // Selector with no arguments
     (
         $macro:path;
         $obj:expr;
-        ($s:ident);
+        ($sel:ident);
         ();
     ) => {{
-        $macro![$obj, $s]
+        $macro![$obj, $sel]
     }};
+
+    // Base case
     (
         $macro:path;
         $obj:expr;
-        ($s1:ident:);
-        ($a1:expr);
+        ();
+        ();
+        $($output:tt)+
     ) => {{
-        $macro![$obj, $s1: $a1]
+        $macro![$obj, $($output)+]
     }};
+
+    // tt-munch each argument
     (
         $macro:path;
         $obj:expr;
-        ($s1:ident: $s2:ident:);
-        ($a1:expr, $a2:expr);
+        ($sel:ident : $($sel_rest:tt)*);
+        ($arg:ident: $arg_ty:ty $(, $($args_rest:tt)*)?);
+        $($output:tt)*
     ) => {{
-        $macro![$obj, $s1: $a1, $s2: $a2]
+        $crate::__collect_msg_send!(
+            $macro;
+            $obj;
+            ($($sel_rest)*);
+            ($($($args_rest)*)?);
+            $($output)*
+            $sel: $arg,
+        )
     }};
-    (
-        $macro:path;
-        $obj:expr;
-        ($s1:ident: $s2:ident: $s3:ident:);
-        ($a1:expr, $a2:expr, $a3:expr);
-    ) => {{
-        $macro![$obj, $s1: $a1, $s2: $a2, $s3: $a3]
-    }};
-    (
-        $macro:path;
-        $obj:expr;
-        ($s1:ident: $s2:ident: $s3:ident: $s4:ident:);
-        ($a1:expr, $a2:expr, $a3:expr, $a4:expr);
-    ) => {{
-        $macro![$obj, $s1: $a1, $s2: $a2, $s3: $a3, $s4: $a4]
-    }};
-    (
-        $macro:path;
-        $obj:expr;
-        ($s1:ident: $s2:ident: $s3:ident: $s4:ident: $s5:ident:);
-        ($a1:expr, $a2:expr, $a3:expr, $a4:expr, $a5:expr);
-    ) => {{
-        $macro![$obj, $s1: $a1, $s2: $a2, $s3: $a3, $s4: $a4, $s5: $a5]
-    }};
-    (
-        $macro:path;
-        $obj:expr;
-        ($s1:ident: $s2:ident: $s3:ident: $s4:ident: $s5:ident: $s6:ident:);
-        ($a1:expr, $a2:expr, $a3:expr, $a4:expr, $a5:expr, $a6:expr);
-    ) => {{
-        $macro![
-            $obj,
-            $s1: $a1,
-            $s2: $a2,
-            $s3: $a3,
-            $s4: $a4,
-            $s5: $a5,
-            $s6: $a6
-        ]
-    }};
-    (
-        $macro:path;
-        $obj:expr;
-        ($s1:ident: $s2:ident: $s3:ident: $s4:ident: $s5:ident: $s6:ident: $s7:ident:);
-        ($a1:expr, $a2:expr, $a3:expr, $a4:expr, $a5:expr, $a6:expr, $a7:expr);
-    ) => {{
-        $macro![
-            $obj,
-            $s1: $a1,
-            $s2: $a2,
-            $s3: $a3,
-            $s4: $a4,
-            $s5: $a5,
-            $s6: $a6,
-            $s7: $a7
-        ]
-    }};
-    (
-        $macro:path;
-        $obj:expr;
-        ($s1:ident: $s2:ident: $s3:ident: $s4:ident: $s5:ident: $s6:ident: $s7:ident: $s8:ident:);
-        ($a1:expr, $a2:expr, $a3:expr, $a4:expr, $a5:expr, $a6:expr, $a7:expr, $a8:expr);
-    ) => {{
-        $macro![
-            $obj,
-            $s1: $a1,
-            $s2: $a2,
-            $s3: $a3,
-            $s4: $a4,
-            $s5: $a5,
-            $s6: $a6,
-            $s7: $a7,
-            $s8: $a8
-        ]
-    }};
-    (
-        $macro:path;
-        $obj:expr;
-        ($s1:ident: $s2:ident: $s3:ident: $s4:ident: $s5:ident: $s6:ident: $s7:ident: $s8:ident: $s9:ident:);
-        ($a1:expr, $a2:expr, $a3:expr, $a4:expr, $a5:expr, $a6:expr, $a7:expr, $a8:expr, $a9:expr);
-    ) => {{
-        $macro![
-            $obj,
-            $s1: $a1,
-            $s2: $a2,
-            $s3: $a3,
-            $s4: $a4,
-            $s5: $a5,
-            $s6: $a6,
-            $s7: $a7,
-            $s8: $a8,
-            $s9: $a9
-        ]
-    }};
-    (
-        $macro:path;
-        $obj:expr;
-        ($s1:ident: $s2:ident: $s3:ident: $s4:ident: $s5:ident: $s6:ident: $s7:ident: $s8:ident: $s9:ident: $s10:ident:);
-        ($a1:expr, $a2:expr, $a3:expr, $a4:expr, $a5:expr, $a6:expr, $a7:expr, $a8:expr, $a9:expr, $a10:expr);
-    ) => {{
-        $macro![
-            $obj,
-            $s1: $a1,
-            $s2: $a2,
-            $s3: $a3,
-            $s4: $a4,
-            $s5: $a5,
-            $s6: $a6,
-            $s7: $a7,
-            $s8: $a8,
-            $s9: $a9,
-            $s10: $a10
-        ]
-    }};
-    (
-        $macro:path;
-        $obj:expr;
-        ($s1:ident: $s2:ident: $s3:ident: $s4:ident: $s5:ident: $s6:ident: $s7:ident: $s8:ident: $s9:ident: $s10:ident: $s11:ident:);
-        ($a1:expr, $a2:expr, $a3:expr, $a4:expr, $a5:expr, $a6:expr, $a7:expr, $a8:expr, $a9:expr, $a10:expr, $a11:expr);
-    ) => {{
-        $macro![
-            $obj,
-            $s1: $a1,
-            $s2: $a2,
-            $s3: $a3,
-            $s4: $a4,
-            $s5: $a5,
-            $s6: $a6,
-            $s7: $a7,
-            $s8: $a8,
-            $s9: $a9,
-            $s10: $a10,
-            $s11: $a11
-        ]
-    }};
-    (
-        $macro:path;
-        $obj:expr;
-        ($s1:ident: $s2:ident: $s3:ident: $s4:ident: $s5:ident: $s6:ident: $s7:ident: $s8:ident: $s9:ident: $s10:ident: $s11:ident: $s12:ident:);
-        ($a1:expr, $a2:expr, $a3:expr, $a4:expr, $a5:expr, $a6:expr, $a7:expr, $a8:expr, $a9:expr, $a10:expr, $a11:expr, $a12:expr);
-    ) => {{
-        $macro![
-            $obj,
-            $s1: $a1,
-            $s2: $a2,
-            $s3: $a3,
-            $s4: $a4,
-            $s5: $a5,
-            $s6: $a6,
-            $s7: $a7,
-            $s8: $a8,
-            $s9: $a9,
-            $s10: $a10,
-            $s11: $a11,
-            $s12: $a12
-        ]
-    }};
+
+    // If couldn't zip selector and arguments, show useful error message
     ($($_any:tt)*) => {{
         compile_error!("Number of arguments in function and selector did not match!")
     }};
