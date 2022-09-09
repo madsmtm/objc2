8249f691edba4148297f6b09f0ff31a25e8938cc
Mads Marquart <mads@marquart.dk>
2022-09-09 08:52:17 +0200

    Parse more things

--- crates/header-translator/src/method.rs
+++ crates/header-translator/src/method.rs
@@ -1,13 +1,26 @@
-use clang::{Entity, EntityKind, TypeKind};
+use clang::{Entity, EntityKind, EntityVisitResult, TypeKind};
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote, ToTokens, TokenStreamExt};
 
 use crate::rust_type::RustType;
 
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+enum MemoryManagement {
+    /// Consumes self and returns retained pointer
+    Init,
+    ReturnsRetained,
+    ReturnsInnerPointer,
+    Normal,
+}
+
+#[allow(dead_code)]
 #[derive(Debug, Clone)]
 pub struct Method {
     selector: String,
     is_class_method: bool,
+    is_optional_protocol_method: bool,
+    memory_management: MemoryManagement,
+    designated_initializer: bool,
     arguments: Vec<(String, RustType)>,
     result_type: Option<RustType>,
 }
@@ -18,8 +31,6 @@ impl Method {
     pub fn parse(entity: Entity<'_>) -> Option<Self> {
         // println!("Method {:?}", entity.get_display_name());
         // println!("Availability: {:?}", entity.get_platform_availability());
-        // TODO: Handle `NSConsumesSelf` and `NSReturnsRetained`
-        // println!("Children: {:?}", entity.get_children());
 
         let selector = entity.get_name().expect("method selector");
 
@@ -53,9 +64,61 @@ impl Method {
             None
         };
 
+        let mut designated_initializer = false;
+        let mut consumes_self = false;
+        let mut memory_management = MemoryManagement::Normal;
+
+        entity.visit_children(|entity, _parent| {
+            match entity.get_kind() {
+                EntityKind::ObjCClassRef
+                | EntityKind::ObjCProtocolRef
+                | EntityKind::TypeRef
+                | EntityKind::ParmDecl => {
+                    // Ignore
+                }
+                EntityKind::ObjCDesignatedInitializer => {
+                    if designated_initializer {
+                        panic!("encountered ObjCDesignatedInitializer twice");
+                    }
+                    designated_initializer = true;
+                }
+                EntityKind::NSConsumesSelf => {
+                    consumes_self = true;
+                }
+                EntityKind::NSReturnsRetained => {
+                    if memory_management != MemoryManagement::Normal {
+                        panic!("got unexpected NSReturnsRetained")
+                    }
+                    memory_management = MemoryManagement::ReturnsRetained;
+                }
+                EntityKind::ObjCReturnsInnerPointer => {
+                    if memory_management != MemoryManagement::Normal {
+                        panic!("got unexpected ObjCReturnsInnerPointer")
+                    }
+                    memory_management = MemoryManagement::ReturnsInnerPointer;
+                }
+                EntityKind::NSConsumed => {
+                    // TODO: Handle consumed arguments
+                }
+                EntityKind::UnexposedAttr => {}
+                _ => panic!("Unknown method child: {:?}, {:?}", entity, _parent),
+            };
+            EntityVisitResult::Recurse
+        });
+
+        if consumes_self {
+            if memory_management != MemoryManagement::ReturnsRetained {
+                panic!("got NSConsumesSelf without NSReturnsRetained");
+            }
+            memory_management = MemoryManagement::Init;
+        }
+
         Some(Self {
             selector,
             is_class_method,
+            is_optional_protocol_method: entity.is_objc_optional(),
+            memory_management,
+            designated_initializer,
             arguments,
             result_type,
         })
