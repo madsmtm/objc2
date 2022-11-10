fb9fc6dd66cef864a7926cf35dcd8340badbd306
Mads Marquart <mads@marquart.dk>
2022-11-10 18:47:14 +0100

    Fix fuzzing on GNUStep

--- fuzz/fuzz_targets/class.rs
+++ fuzz/fuzz_targets/class.rs
@@ -4,6 +4,11 @@ use objc2::runtime::Class;
 use std::ffi::CString;
 
 fuzz_target!(|s: &str| {
+    #[cfg(feature = "gnustep-1-7")]
+    unsafe {
+        objc2::__gnustep_hack::get_class_to_force_linkage()
+    };
+
     if CString::new(s).is_ok() {
         #[allow(clippy::eq_op)]
         if let Some(cls) = Class::get(s) {
--- fuzz/fuzz_targets/sel.rs
+++ fuzz/fuzz_targets/sel.rs
@@ -4,6 +4,11 @@ use objc2::runtime::Sel;
 use std::ffi::CString;
 
 fuzz_target!(|s: &str| {
+    #[cfg(feature = "gnustep-1-7")]
+    unsafe {
+        objc2::__gnustep_hack::get_class_to_force_linkage()
+    };
+
     #[allow(clippy::eq_op)]
     if CString::new(s).is_ok() {
         let sel = Sel::register(s);
