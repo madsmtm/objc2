5f1f99bc838146b383e22ea70686e79e48def6fc
Mads Marquart <mads@marquart.dk>
2023-01-10 17:28:17 +0100

    Properly emit cargo features

--- crates/header-translator/src/output.rs
+++ crates/header-translator/src/output.rs
@@ -1,6 +1,8 @@
 use std::collections::BTreeMap;
 
 use crate::library::Library;
+use crate::stmt::Stmt;
+use crate::config::Config;
 
 #[derive(Debug, PartialEq)]
 pub struct Output {
@@ -25,5 +27,38 @@ impl Output {
                 self_library.compare(other_library);
             },
         );
+    }
+
+    pub fn cargo_features(&self, _config: &Config) -> BTreeMap<String, Vec<String>> {
+        let mut features = BTreeMap::new();
+
+        // for (_, library) in &config.libraries {
+        //
+        // }
+
+        for (_, library) in &self.libraries {
+            for (_, file) in &library.files {
+                for stmt in &file.stmts {
+                    match stmt {
+                        Stmt::ClassDecl {
+                            ty, superclasses, ..
+                        } => {
+                            // Only require the first superclass as feature,
+                            // since the rest will be enabled transitively.
+                            if let Some(superclass) = superclasses.first() {
+                                let feature = format!("{}_{}", ty.library, ty.name);
+                                let superclass_feature = format!("{}_{}", superclass.library, superclass.name);
+                                if let Some(existing) = features.insert(feature, vec![superclass_feature]) {
+                                    error!(?existing, "duplicate feature");
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+
+        features
     }
 }
