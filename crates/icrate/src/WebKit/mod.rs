44dd762c36be174f37295a48c35a1f6eb3e1000d
silvanshade <silvanshade@users.noreply.github.com>
2023-01-21 19:24:58 -0700

    Add fix for WKNavigationAction::sourceFrame

--- crates/icrate/src/WebKit/mod.rs
+++ crates/icrate/src/WebKit/mod.rs
@@ -1,6 +1,9 @@
+mod fixes;
 #[path = "../generated/WebKit/mod.rs"]
 mod generated;
 
+#[allow(unreachable_pub)]
+pub use self::fixes::*;
 pub use self::generated::*;
 
 #[link(name = "WebKit", kind = "framework")]
