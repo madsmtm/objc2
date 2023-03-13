071ac2af368ed80b523878ab886a4fcaef02e250
Mads Marquart <mads@marquart.dk>
2023-03-13 22:30:21 +0300

    Update version

--- objc2/src/lib.rs
+++ objc2/src/lib.rs
@@ -175,7 +175,7 @@
 #![warn(clippy::cargo)]
 #![warn(clippy::ptr_as_ptr)]
 // Update in Cargo.toml as well.
-#![doc(html_root_url = "https://docs.rs/objc2/0.3.0-beta.3")]
+#![doc(html_root_url = "https://docs.rs/objc2/0.3.0-beta.3.patch-leaks")]
 
 #[cfg(not(feature = "alloc"))]
 compile_error!("The `alloc` feature currently must be enabled.");
