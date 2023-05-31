18e5ad59a0f055102274c7e5dff7806bdd322156
Alexander Ekdahl <alexander@ekdahl.io>
2023-05-31 13:23:20 +0200

    Add test

--- crates/icrate/src/MLCompute/mod.rs
+++ crates/icrate/src/MLCompute/mod.rs
@@ -5,3 +5,21 @@ pub use self::generated::*;
 
 #[link(name = "MLCompute", kind = "framework")]
 extern "C" {}
+
+#[cfg(test)]
+#[cfg(feature = "Foundation_NSNumber")]
+#[cfg(feature = "Foundation_NSArray")]
+mod tests {
+    use super::*;
+    use crate::Foundation::{NSArray, NSNumber};
+
+    #[test]
+    fn it_works() {
+        unsafe {
+            MLCTensor::tensorWithShape_dataType(
+                &NSArray::from_vec(vec![NSNumber::new_u8(4), NSNumber::new_u8(4)]),
+                MLCDataTypeFloat32,
+            );
+        }
+    }
+}
