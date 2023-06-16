be1f1d9c55b33622d3c48cd85bd1917db258073a
Mads Marquart <mads@marquart.dk>
2023-06-16 17:17:31 +0300

    Add partial support for optimized objc_retain/objc_release on Aarch64

--- crates/objc2/src/rc/id.rs
+++ crates/objc2/src/rc/id.rs
@@ -7,6 +7,7 @@ use core::ptr::{self, NonNull};
 
 use super::AutoreleasePool;
 use crate::mutability::{IsIdCloneable, IsMutable};
+use crate::runtime::{objc_release_fast, objc_retain_fast};
 use crate::{ffi, ClassType, Message};
 
 /// A reference counted pointer type for Objective-C objects.
@@ -321,7 +322,7 @@ impl<T: Message> Id<T> {
     #[inline]
     pub unsafe fn retain(ptr: *mut T) -> Option<Id<T>> {
         // SAFETY: The caller upholds that the pointer is valid
-        let res: *mut T = unsafe { ffi::objc_retain(ptr.cast()) }.cast();
+        let res: *mut T = unsafe { objc_retain_fast(ptr.cast()) }.cast();
         debug_assert_eq!(res, ptr, "objc_retain did not return the same pointer");
         // SAFETY: We just retained the object, so it has +1 retain count
         unsafe { Self::new(res) }
@@ -650,7 +651,7 @@ impl<T: ?Sized> Drop for Id<T> {
 
         // SAFETY: The `ptr` is guaranteed to be valid and have at least one
         // retain count.
-        unsafe { ffi::objc_release(self.ptr.as_ptr().cast()) };
+        unsafe { objc_release_fast(self.ptr.as_ptr().cast()) };
     }
 }
 
