6fcb0c7743cf56da30eb588328557dc136b5ca29
Mads Marquart <mads@marquart.dk>
2023-08-27 23:33:45 +0200

    Make assembly test script simpler

--- helper-scripts/run-assembly-tests.sh
+++ helper-scripts/run-assembly-tests.sh
@@ -4,30 +4,29 @@
 
 set -euxo pipefail
 
-ARGS="run --features=run --bin=test-assembly --  -Zbuild-std --target"
-
-# Add `+nightly` when using `-Zbuild-std` if the user's `cargo` is not a
-# `nightly` by default
+# Add `+nightly` if the user's `cargo` is not a `nightly` already
 if grep -q nightly <<< $(cargo --version); then
     NIGHTLY=""
 else
     NIGHTLY="+nightly"
 fi
 
+RUN="cargo $NIGHTLY run --bin=test-assembly --  -Zbuild-std -Zbuild-std --target"
+
 echo "Apple"
-cargo $NIGHTLY $ARGS x86_64-apple-darwin
-cargo $NIGHTLY $ARGS aarch64-apple-darwin
-cargo $NIGHTLY $ARGS armv7s-apple-ios
-cargo $NIGHTLY $ARGS armv7-apple-ios
-cargo $NIGHTLY $ARGS i386-apple-ios
+$RUN x86_64-apple-darwin
+$RUN aarch64-apple-darwin
+$RUN armv7s-apple-ios
+$RUN armv7-apple-ios
+$RUN i386-apple-ios
 
 echo "Old Apple"
-cargo $NIGHTLY $ARGS i686-apple-darwin
+$RUN i686-apple-darwin
 
 echo "GNUStep"
-cargo $NIGHTLY $ARGS x86_64-unknown-linux-gnu --no-default-features --features=std,gnustep-2-1
-cargo $NIGHTLY $ARGS i686-unknown-linux-gnu --no-default-features --features=std,gnustep-2-1
+$RUN x86_64-unknown-linux-gnu --no-default-features --features=std,gnustep-2-1
+$RUN i686-unknown-linux-gnu --no-default-features --features=std,gnustep-2-1
 
 echo "Old GNUStep"
-cargo $NIGHTLY $ARGS x86_64-unknown-linux-gnu --no-default-features --features=std,gnustep-1-7
-cargo $NIGHTLY $ARGS i686-unknown-linux-gnu --no-default-features --features=std,gnustep-1-7
+$RUN x86_64-unknown-linux-gnu --no-default-features --features=std,gnustep-1-7
+$RUN i686-unknown-linux-gnu --no-default-features --features=std,gnustep-1-7
