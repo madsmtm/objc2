bf72828fe66f9deb42fc5d4877347118460de33b
Mads Marquart <mads@marquart.dk>
2024-02-15 05:12:35 +0100

    Update opaque encoding example
    Since objc2-encode v0.4, this can be simplified

--- crates/objc2/examples/encode_opaque_type.rs
+++ crates/objc2/examples/encode_opaque_type.rs
@@ -3,8 +3,7 @@ use objc2::encode::{Encoding, RefEncode};
 // We choose in this case to represent `NSDecimal` as an opaque struct because
 // we don't know much about the internals.
 //
-// Therefore we do not implement `Encode`, but when implementing `RefEncode`,
-// the type-encoding still has to be correct.
+// Therefore we do not implement `Encode`.
 #[repr(C)]
 struct NSDecimal {
     // Note: This should be an [extern type][rfc-1861] instead, when that
@@ -16,21 +15,13 @@ struct NSDecimal {
 
 // SAFETY: `&NSDecimal` is a valid pointer, and the encoding is correct.
 unsafe impl RefEncode for NSDecimal {
-    // Running `@encode` on `NSDecimal*` on my 64-bit system gives
-    // `^{?=cCCC[38C]}`. On other systems it may be something else!
-    const ENCODING_REF: Encoding = Encoding::Pointer(&Encoding::Struct(
-        "?",
-        &[
-            Encoding::Char,
-            Encoding::UChar,
-            Encoding::UChar,
-            Encoding::UChar,
-            Encoding::Array(38, &Encoding::UChar),
-        ],
-    ));
+    const ENCODING_REF: Encoding = Encoding::Pointer(&Encoding::Struct("?", &[]));
 }
 
 fn main() {
+    // Running `@encode` on `NSDecimal*` on my 64-bit system gives
+    // `^{?=cCCC[38C]}`, but empty structs are treated as equivalent to all
+    // other structs by `objc2`.
     assert!(NSDecimal::ENCODING_REF.equivalent_to_str("^{?=cCCC[38C]}"));
     // Does not compile:
     // println!("{:?}", NSDecimal::ENCODING);
