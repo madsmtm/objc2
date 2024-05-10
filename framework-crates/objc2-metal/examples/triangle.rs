ac975b28c79a7f637532ce1cf9fe3cdedf580999
Mads Marquart <mads@marquart.dk>
2024-05-10 11:09:21 +0200

    Refactor Metal triangle example shader out as a separate file

--- framework-crates/objc2-metal/examples/triangle.rs
+++ framework-crates/objc2-metal/examples/triangle.rs
@@ -22,48 +22,6 @@ use objc2_metal::{
 };
 use objc2_metal_kit::{MTKView, MTKViewDelegate};
 
-#[rustfmt::skip]
-const SHADERS: &str = r#"
-    #include <metal_stdlib>
-
-    struct SceneProperties {
-        float time;
-    };
-
-    struct VertexInput {
-        metal::packed_float3 position;
-        metal::packed_float3 color;
-    };
-
-    struct VertexOutput {
-        metal::float4 position [[position]];
-        metal::float4 color;
-    };
-
-    vertex VertexOutput vertex_main(
-        device const SceneProperties& properties [[buffer(0)]],
-        device const VertexInput* vertices [[buffer(1)]],
-        uint vertex_idx [[vertex_id]]
-    ) {
-        VertexOutput out;
-        VertexInput in = vertices[vertex_idx];
-        out.position =
-            metal::float4(
-                metal::float2x2(
-                    metal::cos(properties.time), -metal::sin(properties.time),
-                    metal::sin(properties.time),  metal::cos(properties.time)
-                ) * in.position.xy,
-                in.position.z,
-                1);
-        out.color = metal::float4(in.color, 1);
-        return out;
-    }
-
-    fragment metal::float4 fragment_main(VertexOutput in [[stage_in]]) {
-        return in.color;
-    }
-"#;
-
 #[derive(Copy, Clone)]
 #[repr(C)]
 struct SceneProperties {
@@ -179,7 +137,7 @@ declare_class!(
 
             // compile the shaders
             let library = device
-                .newLibraryWithSource_options_error(ns_string!(SHADERS), None)
+                .newLibraryWithSource_options_error(ns_string!(include_str!("triangle.metal")), None)
                 .expect("Failed to create a library.");
 
             // configure the vertex shader
