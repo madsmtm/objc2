eb39164bcf67c8e8328cfe493b7be37a9e5101a7
Mads Marquart <mads@marquart.dk>
2024-05-17 17:32:16 +0200

    Add documentation on core-foundation interop

--- crates/objc2/src/topics/mod.rs
+++ crates/objc2/src/topics/mod.rs
@@ -2,5 +2,8 @@
 
 pub mod about_generated;
 
+#[cfg(not(feature = "gnustep-1-7"))]
+#[doc = include_str!("core_foundation_interop.md")]
+pub mod core_foundation_interop {}
 #[doc = include_str!("layered_safety.md")]
 pub mod layered_safety {}
