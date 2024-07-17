975ac51177a5c0e45fef2b79662ae53c13303eab
Paul Mabileau <paul.mabileau@harfanglab.fr>
2024-07-17 20:01:30 +0200

    Test(ui): Extend block tests for with_encoding
    Signed-off-by: Paul Mabileau <paul.mabileau@harfanglab.fr>

--- crates/test-ui/ui/lifetime_of_closure_tied_to_block.rs
+++ crates/test-ui/ui/lifetime_of_closure_tied_to_block.rs
@@ -1,4 +1,12 @@
-use block2::{RcBlock, StackBlock};
+use block2::{ManualBlockEncoding, RcBlock, StackBlock};
+use std::ffi::CStr;
+
+struct VoidToI32;
+unsafe impl ManualBlockEncoding for VoidToI32 {
+    type Arguments = ();
+    type Return = i32;
+    const ENCODING_CSTR: &'static CStr = c"i8@?0";
+}
 
 fn main() {
     let _ = {
@@ -13,11 +21,31 @@ fn main() {
 
     let _ = {
         let x = 2;
+        unsafe { RcBlock::with_encoding::<_, _, _, VoidToI32>(|| x + 2) }
+    };
+
+    let _ = {
+        let x = 2;
+        unsafe { RcBlock::with_encoding::<_, _, _, VoidToI32>(|| x + 2).clone() }
+    };
+
+    let _ = {
+        let x = 2;
         StackBlock::new(|| x + 2)
     };
 
     let _ = {
         let x = 2;
         StackBlock::new(|| x + 2).copy()
+    };
+
+    let _ = {
+        let x = 2;
+        unsafe { StackBlock::with_encoding::<VoidToI32>(|| x + 2) }
+    };
+
+    let _ = {
+        let x = 2;
+        unsafe { StackBlock::with_encoding::<VoidToI32>(|| x + 2).copy() }
     };
 }
