1a57243450809fe120f986f132e4ee4f3425a955
Mads Marquart <mads@marquart.dk>
2024-11-21 18:03:20 +0100

    Rename internal CachedId -> CachedRetained

--- framework-crates/objc2-foundation/src/__macro_helpers/mod.rs
+++ framework-crates/objc2-foundation/src/__macro_helpers/mod.rs
@@ -2,6 +2,6 @@ mod cached;
 #[cfg(feature = "NSString")]
 mod ns_string;
 
-pub use self::cached::CachedId;
+pub use self::cached::CachedRetained;
 #[cfg(feature = "NSString")]
 pub use self::ns_string::*;
