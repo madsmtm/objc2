4e0c52d39cadaa7e64f56018927d989ab80632fa
Mads Marquart <mads@marquart.dk>
2025-02-17 15:25:52 +0100

    Document MainThreadMarker further
    Carry over extra documentation I wrote when attempting to upstream this
    to the standard library: https://github.com/rust-lang/rust/pull/136616.

--- crates/objc2/src/main_thread_marker.rs
+++ crates/objc2/src/main_thread_marker.rs
@@ -1,3 +1,5 @@
+//! This belongs in `std` IMO, but wasn't accepted there:
+//! <https://github.com/rust-lang/rust/pull/136616>
 use core::fmt;
 use core::marker::PhantomData;
 
@@ -26,7 +28,20 @@ fn is_main_thread() -> bool {
         }
 
         // SAFETY: Can be called from any thread.
-        unsafe { pthread_main_np() != 0 }
+        //
+        // Apple's man page says:
+        // > The pthread_main_np() function returns 1 if the calling thread is the initial thread, 0 if
+        // > the calling thread is not the initial thread, and -1 if the thread's initialization has not
+        // > yet completed.
+        //
+        // However, Apple's header says:
+        // > Returns non-zero if the current thread is the main thread.
+        //
+        // So unclear if we should be doing a comparison against 1, or a negative comparison against 0?
+        // To be safe, we compare against 1, though in reality, the current implementation can only ever
+        // return 0 or 1:
+        // https://github.com/apple-oss-distributions/libpthread/blob/libpthread-535/src/pthread.c#L1084-L1089
+        unsafe { pthread_main_np() == 1 }
     }
 
     #[cfg(not(target_vendor = "apple"))]
@@ -37,20 +52,33 @@ fn is_main_thread() -> bool {
     }
 }
 
-/// A marker type taken by functions that can only be executed on the main
-/// thread.
+/// A marker type for functionality only available on the main thread.
 ///
-/// By design, this is neither [`Send`] nor [`Sync`], and can only be created
-/// on the main thread, meaning that if you're holding this, you know you're
-/// running on the main thread.
+/// The main thread is a system-level property on Apple/Darwin platforms, and
+/// has extra capabilities not available on other threads. This is usually
+/// relevant when using native GUI frameworks, where most operations must be
+/// done on the main thread.
+///
+/// This type enables you to manage that capability. By design, it is neither
+/// [`Send`] nor [`Sync`], and can only be created on the main thread, meaning
+/// that if you have an instance of this, you are guaranteed to be on the main
+/// thread / have the "main-thread capability".
+///
+/// [The `main` function][main-functions] will run on the main thread. This
+/// type can also be used with `#![no_main]` or other such cases where Rust is
+/// not defining the binary entry point.
 ///
 /// See the following links for more information on main-thread-only APIs:
-/// - [Main Thread Only APIs on OS X](https://www.dribin.org/dave/blog/archives/2009/02/01/main_thread_apis/)
-/// - [Thread Safety Summary](https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/Multithreading/ThreadSafetySummary/ThreadSafetySummary.html#//apple_ref/doc/uid/10000057i-CH12-SW1)
 /// - [Are the Cocoa Frameworks Thread Safe?](https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/CocoaFundamentals/AddingBehaviortoaCocoaProgram/AddingBehaviorCocoa.html#//apple_ref/doc/uid/TP40002974-CH5-SW47)
+/// - [About Threaded Programming](https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/Multithreading/AboutThreads/AboutThreads.html)
+/// - [Thread Safety Summary](https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/Multithreading/ThreadSafetySummary/ThreadSafetySummary.html#//apple_ref/doc/uid/10000057i-CH12-SW1)
 /// - [Technical Note TN2028 - Threading Architectures](https://developer.apple.com/library/archive/technotes/tn/tn2028.html#//apple_ref/doc/uid/DTS10003065)
 /// - [Thread Management](https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/Multithreading/CreatingThreads/CreatingThreads.html)
+/// - [Swift's `@MainActor`](https://developer.apple.com/documentation/swift/mainactor)
+/// - [Main Thread Only APIs on OS X](https://www.dribin.org/dave/blog/archives/2009/02/01/main_thread_apis/)
 /// - [Mike Ash' article on thread safety](https://www.mikeash.com/pyblog/friday-qa-2009-01-09.html)
+///
+/// [main-functions]: https://doc.rust-lang.org/reference/crates-and-source-files.html#main-functions
 ///
 ///
 /// # Main Thread Checker
@@ -86,37 +114,115 @@ fn is_main_thread() -> bool {
 ///
 /// # Examples
 ///
-/// Use when designing APIs that are only safe to use on the main thread:
+/// Retrieve the main thread marker in different situations.
+///
+/// ```
+/// use objc2::MainThreadMarker;
+///
+/// # // explicitly uses `fn main`
+/// fn main() {
+///     // The thread that `fn main` runs on is the main thread.
+///     assert!(MainThreadMarker::new().is_some());
+///
+///     // Subsequently spawned threads are not the main thread.
+///     std::thread::spawn(|| {
+///         assert!(MainThreadMarker::new().is_none());
+///     }).join().unwrap();
+/// }
+/// ```
+///
+/// Use when accessing APIs that are only safe to use on the main thread.
 ///
 /// ```no_run
-/// use objc2::{MainThreadMarker, msg_send};
-/// use objc2::runtime::NSObject;
-/// # let obj = 0 as *const NSObject;
+/// use objc2::MainThreadMarker;
+/// # #[cfg(needs_app_kit)]
+/// use objc2_app_kit::NSApplication;
+/// #
+/// # use objc2::runtime::NSObject as NSApplication;
+/// # trait Foo {
+/// #     fn sharedApplication(_mtm: MainThreadMarker) {}
+/// # }
+/// # impl Foo for NSApplication {}
 ///
-/// // This action requires the main thread, so we take a marker as parameter.
-/// // It signals clearly to users "this requires the main thread".
-/// unsafe fn do_thing(obj: *const NSObject, _mtm: MainThreadMarker) {
-///     msg_send![obj, someActionThatRequiresTheMainThread]
+/// # // explicitly uses `fn main`
+/// fn main() {
+///     // Create a new MainThreadMarker.
+///     let mtm = MainThreadMarker::new().expect("must be on the main thread");
+///
+///     // NSApplication is only usable on the main thread,
+///     // so we need to pass the marker as an argument.
+///     let app = NSApplication::sharedApplication(mtm);
+///
+///     // Do something with the application
+///     // app.run();
+/// }
+/// ```
+///
+/// Create a static that is only usable on the main thread. This is similar to
+/// a thread-local, but can be more efficient because it doesn't handle
+/// multiple threads.
+///
+/// See also `dispatch2::MainThreadBound`.
+///
+/// ```
+/// use objc2::MainThreadMarker;
+/// use std::cell::UnsafeCell;
+///
+/// struct SyncUnsafeCell<T>(UnsafeCell<T>);
+///
+/// unsafe impl<T> Sync for SyncUnsafeCell<T> {}
+///
+/// static MAIN_THREAD_ONLY_VALUE: SyncUnsafeCell<i32> = SyncUnsafeCell(UnsafeCell::new(0));
+///
+/// fn set(value: i32, _mtm: MainThreadMarker) {
+///     // SAFETY: We have an instance of `MainThreadMarker`, so we know that
+///     // we're running on the main thread (and thus do not need any
+///     // synchronization, since the only accesses to this value is from the
+///     // main thread).
+///     unsafe { *MAIN_THREAD_ONLY_VALUE.0.get() = value };
 /// }
 ///
-/// // Usage
+/// fn get(_mtm: MainThreadMarker) -> i32 {
+///     // SAFETY: Same as above.
+///     unsafe { *MAIN_THREAD_ONLY_VALUE.0.get() }
+/// }
 ///
-/// // Create a new MainThreadMarker.
-/// let mtm = MainThreadMarker::new().expect("must be on the main thread");
-/// unsafe { do_thing(obj, mtm) }
+/// # // explicitly uses `fn main`
+/// fn main() {
+///     let mtm = MainThreadMarker::new().expect("must be on the main thread");
+///     set(42, mtm);
+///     assert_eq!(get(mtm), 42);
+/// }
 /// ```
 #[derive(Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
-// This is valid to Copy because it's still `!Send` and `!Sync`.
+//              ^^^^ this is valid because it's still `!Send` and `!Sync`.
 pub struct MainThreadMarker {
     // No lifetime information needed; the main thread is static and available
     // throughout the entire program!
+
+    // Ensure `!Send` and `!Sync`.
     _priv: PhantomData<*mut ()>,
 }
 
 impl MainThreadMarker {
-    /// Construct a new [`MainThreadMarker`].
+    /// Construct a new `MainThreadMarker`.
     ///
     /// Returns [`None`] if the current thread was not the main thread.
+    ///
+    ///
+    /// # Example
+    ///
+    /// Check whether the current thread is the main thread.
+    ///
+    /// ```
+    /// use objc2::MainThreadMarker;
+    ///
+    /// if MainThreadMarker::new().is_some() {
+    ///     // Is the main thread
+    /// } else {
+    ///     // Not the main thread
+    /// }
+    /// ```
     #[inline]
     #[doc(alias = "is_main_thread")]
     #[doc(alias = "pthread_main_np")]
@@ -130,8 +236,8 @@ impl MainThreadMarker {
         }
     }
 
-    /// Construct a new [`MainThreadMarker`] without first checking whether
-    /// the current thread is the main one.
+    /// Construct a new `MainThreadMarker` without first checking whether the
+    /// current thread is the main one.
     ///
     ///
     /// # Safety
@@ -144,10 +250,13 @@ impl MainThreadMarker {
     /// another thread than the main one.
     #[inline]
     pub const unsafe fn new_unchecked() -> Self {
-        // SAFETY: Upheld by caller
+        // SAFETY: Upheld by caller.
         //
-        // We can't debug_assert that this actually is the main thread, see
-        // the comment above.
+        // We can't debug_assert that this actually is the main thread, both
+        // because this is `const` (to allow usage in `static`s), and because
+        // users may sometimes want to create this briefly, e.g. to access an
+        // API that in most cases requires the marker, but is safe to use
+        // without in specific cases.
         Self { _priv: PhantomData }
     }
 
