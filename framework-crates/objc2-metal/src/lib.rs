362c85df32bfe605861e2afc169e23d7d5149e17
Marijn Suijten <marijn@traverseresearch.nl>
2025-04-29 15:06:05 +0200

    Metal: Misc non-`unsafe` translation fixes

--- framework-crates/objc2-metal/src/lib.rs
+++ framework-crates/objc2-metal/src/lib.rs
@@ -11,7 +11,7 @@
 //!
 //! [apple-doc]: https://developer.apple.com/documentation/xcode/validating-your-apps-metal-api-usage/.
 //!
-//! NOTE: To use [`MTLCreateSystemDefaultDevice`] you need to link to
+//! NOTE: To use [`MTLCreateSystemDefaultDevice()`] you need to link to
 //! `CoreGraphics`, this can be done by using `objc2-core-graphics`, or by
 //! doing:
 //! ```rust
