6a3ae136c581243743f3de56195b5a3a0c463c21
Mads Marquart <mads@marquart.dk>
2025-08-16 07:16:42 +0200

    Properly fix CI for default_xcode_game
    Annoying that there isn't a better way to specify this to Cargo.

--- examples/metal/default_xcode_game/main.rs
+++ examples/metal/default_xcode_game/main.rs
@@ -6,17 +6,24 @@
 //!
 //! NOTE: Using a Storyboard outside of Xcode is quite involved, so instead,
 //! we set up the entire UI (menubar and window) ourselves.
-#![cfg(all(target_os = "macos", target_arch = "aarch64"))]
 
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
 use objc2::MainThreadMarker;
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
 use objc2_app_kit::NSApplication;
 
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
 mod app_delegate;
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
 mod game_view_controller;
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
 mod matrix;
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
 mod renderer;
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
 mod shader_types;
 
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
 fn main() {
     let mtm = MainThreadMarker::new().unwrap();
     objc2::rc::autoreleasepool(|_| {
@@ -24,4 +31,9 @@ fn main() {
         app_delegate::set_application_delegate(&app);
         app.run();
     });
+}
+
+#[cfg(not(all(target_os = "macos", target_arch = "aarch64")))]
+fn main() {
+    panic!("unsupported platform in this example");
 }
