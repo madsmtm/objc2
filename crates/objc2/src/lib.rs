244fdaa01684381d05361376d84842f61dbad85c
Mads Marquart <mads@marquart.dk>
2026-04-17 07:56:52 +0200

    Update to Xcode 26.4.1
    There's no SDK changes in this version, so this is mostly a formality.

--- crates/objc2/src/lib.rs
+++ crates/objc2/src/lib.rs
@@ -63,17 +63,17 @@
 //!
 //! ## Supported operating systems
 //!
-//! - macOS: `10.12-26.4`
-//! - iOS: `10.0-26.4` (including iPadOS and Mac Catalyst)
-//! - tvOS: `10.0-26.4`
-//! - watchOS: `5.0-26.4`
-//! - visionOS: `1.0-26.4`
+//! - macOS: `10.12-26.4.1`
+//! - iOS: `10.0-26.4.1` (including iPadOS and Mac Catalyst)
+//! - tvOS: `10.0-26.4.1`
+//! - watchOS: `5.0-26.4.1`
+//! - visionOS: `1.0-26.4.1`
 //!
 //! The minimum versions are the same as those supported by `rustc`. Higher
 //! versions will also work, but the framework crates will not have bindings
 //! available for newer APIs.
 //!
-//! The framework bindings are generated from the SDKs in Xcode 26.4. The
+//! The framework bindings are generated from the SDKs in Xcode 26.4.1. The
 //! Xcode version are updated usually within a week of [GitHub Actions]
 //! supporting the new Xcode version, and we try to schedule crate releases
 //! such that align fairly closely with Xcode updates. We only support stable
--- crates/objc2/src/topics/FRAMEWORKS_CHANGELOG.md
+++ crates/objc2/src/topics/FRAMEWORKS_CHANGELOG.md
@@ -29,13 +29,14 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
   - `Network` / `objc2-network`.
 
 ### Changed
-* Updated SDK from Xcode 26.0.1 to 26.4.
+* Updated SDK from Xcode 26.0.1 to 26.4.1.
 
   View the release notes to learn more details:
   - [26.1.1](https://developer.apple.com/documentation/xcode-release-notes/xcode-26_1-release-notes)
   - [26.2](https://developer.apple.com/documentation/xcode-release-notes/xcode-26_2-release-notes)
   - [26.3](https://developer.apple.com/documentation/xcode-release-notes/xcode-26_3-release-notes)
   - [26.4](https://developer.apple.com/documentation/xcode-release-notes/xcode-26_4-release-notes)
+  - [26.4.1](https://developer.apple.com/documentation/xcode-release-notes/xcode-26_4_1-release-notes)
 
   Breaking changes are noted elsewhere in this changelog entry.
 * Changed the value of `kUSBHostPortPropertyPortNumber`.
