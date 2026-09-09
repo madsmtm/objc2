2a8387ae2a2bdfa7c789a63e1f921ff4528a9e90
Mads Marquart <mads@marquart.dk>
2026-09-09 23:42:01 +0200

    Refactor header-translator/lib.rs

---
+++ crates/header-translator/src/command.rs
@@ -0,0 +1,39 @@
+use std::fmt;
+use std::path::Path;
+use std::process::{Command, Stdio};
+
+pub fn run_cargo_fmt(packages: impl IntoIterator<Item = impl fmt::Display>) {
+    let status = Command::new("cargo")
+        .arg("fmt")
+        .args(packages.into_iter().map(|package| format!("-p{package}")))
+        .current_dir(Path::new(env!("CARGO_MANIFEST_DIR")).parent().unwrap())
+        .status()
+        .expect("failed running cargo fmt");
+
+    assert!(
+        status.success(),
+        "failed running cargo fmt with exit code {status}"
+    );
+}
+
+pub fn run_rustfmt(data: impl fmt::Display) -> Vec<u8> {
+    use std::io::Write;
+
+    let mut child = Command::new("rustfmt")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed running rustfmt");
+
+    let mut stdin = child.stdin.take().expect("failed to open stdin");
+    write!(stdin, "{data}").expect("failed writing");
+    drop(stdin);
+
+    let output = child.wait_with_output().expect("failed formatting");
+
+    if !output.status.success() {
+        panic!("failed running rustfmt with exit code {}", output.status)
+    }
+
+    output.stdout
+}
--- crates/header-translator/src/lib.rs
+++ crates/header-translator/src/lib.rs
@@ -3,14 +3,9 @@
 #[macro_use]
 extern crate tracing;
 
-use std::fmt::{self, Display};
-use std::path::Path;
-use std::process::{Command, Stdio};
-
-use clang::{Entity, EntityVisitResult};
-use tracing::span::EnteredSpan;
-
 mod availability;
+mod clang_utils;
+mod command;
 mod context;
 mod display_helper;
 pub mod documentation;
@@ -29,6 +24,8 @@ mod thread_safety;
 mod unexposed_attr;
 
 pub use self::availability::HOST_MACOS;
+pub use self::clang_utils::immediate_children;
+pub use self::command::{run_cargo_fmt, run_rustfmt};
 pub use self::context::{Context, MacroEntity, MacroLocation};
 pub use self::documentation::EXTRA_BLOCK_COMMANDS;
 pub use self::global_analysis::global_analysis;
@@ -36,57 +33,3 @@ pub use self::id::{ItemIdentifier, Location};
 pub use self::library::Library;
 pub use self::module::Module;
 pub use self::stmt::Stmt;
-
-pub fn run_cargo_fmt(packages: impl IntoIterator<Item = impl Display>) {
-    let status = Command::new("cargo")
-        .arg("fmt")
-        .args(packages.into_iter().map(|package| format!("-p{package}")))
-        .current_dir(Path::new(env!("CARGO_MANIFEST_DIR")).parent().unwrap())
-        .status()
-        .expect("failed running cargo fmt");
-
-    assert!(
-        status.success(),
-        "failed running cargo fmt with exit code {status}"
-    );
-}
-
-pub fn run_rustfmt(data: impl fmt::Display) -> Vec<u8> {
-    use std::io::Write;
-
-    let mut child = Command::new("rustfmt")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("failed running rustfmt");
-
-    let mut stdin = child.stdin.take().expect("failed to open stdin");
-    write!(stdin, "{data}").expect("failed writing");
-    drop(stdin);
-
-    let output = child.wait_with_output().expect("failed formatting");
-
-    if !output.status.success() {
-        panic!("failed running rustfmt with exit code {}", output.status)
-    }
-
-    output.stdout
-}
-
-fn immediate_children<'tu>(
-    entity: &Entity<'tu>,
-    mut closure: impl FnMut(Entity<'tu>, EnteredSpan),
-) {
-    entity.visit_children(|entity, _parent| {
-        let span = debug_span!(
-            "child",
-            kind = ?entity.get_kind(),
-            dbg = entity.get_name(),
-        )
-        .entered();
-
-        closure(entity, span);
-
-        EntityVisitResult::Continue
-    });
-}
